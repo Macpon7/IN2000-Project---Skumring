703646c8fd555b97dd94d4b44bb39102d54277d6
Adrian Chambe-Eng <adrianch@ifi.uio.no>
2024-02-23 09:01:13 +0100

    Initial Commit

---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/MainActivity.kt
@@ -0,0 +1,46 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.theme.SkumringTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SkumringTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SkumringTheme {
+        Greeting("Android")
+    }
+}
