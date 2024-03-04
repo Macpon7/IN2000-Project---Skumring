f05b74382929490764d45a2bc95dd92825ff07fa
viljardh <viljardh@uio.no>
2024-03-04 09:55:36 +0100

    no rart med exampleunittest, lar den være

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/locationforecast/LocationForecastDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/locationforecast/LocationForecastDataSource.kt
@@ -1,5 +1,6 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.data.locationforecast
 import android.util.Log
+import com.google.gson.Gson
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -9,9 +10,10 @@ import java.net.UnknownHostException
 import io.ktor.serialization.gson.gson
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.locationforecast.LocationForecastInfo
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.locationforecast.Properties
+import java.io.File
 
 
-class LocationForecastDataSource (private val path: String = "https://api.met.no/weatherapi/locationforecast/2.0/edr/collections/complete/position?coords=POINT%2810%2060%29\n"){
+class LocationForecastDataSource (private val path: String = "https://api.met.no/weatherapi/locationforecast/2.0/edr/collections/complete/position?coords=POINT%2810%2060%29"){
     private val client = HttpClient {
         install(ContentNegotiation) {
             gson()
@@ -22,10 +24,4 @@ class LocationForecastDataSource (private val path: String = "https://api.met.no
         val response: HttpResponse = client.get(path)
         return response.body()
     }
-}
-
-suspend fun main() {
-    val source = LocationForecastDataSource()
-    print(source.fetchLocationForecastData())
-
 }
