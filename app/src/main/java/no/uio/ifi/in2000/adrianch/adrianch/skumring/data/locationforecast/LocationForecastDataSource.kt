d890afd8c5e7428024acb0db2c5d315116ea56ce
Adrian Chambe-Eng <adrianch@ifi.uio.no>
2024-03-06 12:03:12 +0100

    Add conversion from API result to our own WeatherPerHour data class

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/locationforecast/LocationForecastDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/locationforecast/LocationForecastDataSource.kt
@@ -6,9 +6,11 @@ import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import io.ktor.serialization.gson.gson
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.locationforecastapi.LocationForecastInfo
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.locationforecastapi.WeatherPerHour
 
 
-class LocationForecastDataSource (private var path: String = "https://api.met.no/weatherapi/locationforecast/2.0/edr/collections/complete/position?"){
+class LocationForecastDataSource (){
+    private var path: String = "https://api.met.no/weatherapi/locationforecast/2.0/edr/collections/complete/position?"
     private val client = HttpClient {
         install(ContentNegotiation) {
             gson()
@@ -19,7 +21,7 @@ class LocationForecastDataSource (private var path: String = "https://api.met.no
      * Returnerer et LocationForecastInfo-objekt som har timeseriesobjektene med
      * værdata etter gitt tid og gitte koordinater.
      */
-    suspend fun fetchLocationForecastData(longitude: String, latitude: String): LocationForecastInfo {
+    private suspend fun fetchLocationForecastData(longitude: String, latitude: String): LocationForecastInfo {
         this.path += "coords=POINT($longitude+$latitude)"
          try {
             val response: HttpResponse = client.get(this.path)
@@ -28,4 +30,20 @@ class LocationForecastDataSource (private var path: String = "https://api.met.no
             throw e
         }
     }
+
+    suspend fun fetchWeatherData(latitude: String, longitude: String): List<WeatherPerHour> {
+        val dataFromAPI = fetchLocationForecastData(latitude = latitude, longitude = longitude)
+
+        return dataFromAPI.properties.timeseries.map {
+            var icon: String?
+            if (it.data.next_1_hours == null) {
+                icon = null
+            } else {
+                icon = it.data.next_1_hours.summary.symbol_code
+            }
+
+            WeatherPerHour(it.time, it.data.instant.details, icon)
+        }
+    }
 }
+
--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/locationforecastapi/WeatherPerHour.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/locationforecastapi/WeatherPerHour.kt
@@ -3,5 +3,5 @@ package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.locationforecastapi
 data class WeatherPerHour (
     val time: String,
     val instant: WeatherDetails,
-    val icon: String
+    val icon: String?
 )
