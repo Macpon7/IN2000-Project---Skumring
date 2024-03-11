a1a8f7b9df765485b598642a921a4897cdf14622
viljardh <viljardh@uio.no>
2024-03-11 10:02:19 +0100

    start getPlaceInfo

---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/placeinfo/PlaceInfoRepository.kt
@@ -0,0 +1,27 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.data.placeinfo
+
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.locationforecast.LocationForecastDataSource
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.sunrise.SunriseDataSource
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.locationforecastapi.WeatherPerHour
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo.PlaceInfo
+
+class PlaceInfoRepository {
+    val sunriseDataSource = SunriseDataSource()
+    val locationDataSource = LocationForecastDataSource()
+
+    suspend fun getPlaceInfo(lat: String, long: String, id: Int = 0): PlaceInfo {
+        //contains data for 10 days currently - might change
+        val fullForecast = locationDataSource.fetchWeatherData(latitude = lat, longitude = long)
+        val forecastGroupedByDate = fullForecast.groupBy { it.time.take(10) }
+
+        forecastGroupedByDate.forEach{
+            val sunActivity = sunriseDataSource.fetchSunActivity(
+                lat = lat,
+                long = long,
+                date = it.value[0].time.take(10)
+            )
+
+        }
+
+    }
+}
---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/placeinfo/SunEvent.kt
@@ -0,0 +1,6 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo
+
+data class SunEvent (
+    val time: String,
+    val conditions: Boolean
+)
