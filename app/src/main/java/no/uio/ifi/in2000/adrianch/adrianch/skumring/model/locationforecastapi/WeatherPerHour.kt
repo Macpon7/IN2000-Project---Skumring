9c5c5c62f02048f9409455b832bba6bf05988f75
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-03-12 16:46:41 +0100

    Change everywhere we use String for date or time to LocalDate and LocalDateTime

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/locationforecastapi/WeatherPerHour.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/locationforecastapi/WeatherPerHour.kt
@@ -1,7 +1,9 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.locationforecastapi
 
+import java.time.LocalDateTime
+
 data class WeatherPerHour (
-    val time: String,
+    val time: LocalDateTime,
     val instant: WeatherDetails,
     val icon: String?
 )
