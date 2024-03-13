d25f69636b4f5b823a864b6aa8c273e5d1abbfc2
Mariisno <marinosve@gmail.com>
2024-03-13 12:16:45 +0100

    add skeleton for buttonnavigationbar

---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/SkumringBottomBar.kt
@@ -0,0 +1,59 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.sharedcomponents
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.home.HomeDestination
+
+@Composable
+fun SkumringBottomBar (
+    navController: NavHostController, state: MutableState<Boolean>, modifier: Modifier = Modifier
+) {
+    val screens = listOf(
+        HomeDestination
+    )
+
+    NavigationBar(
+        modifier = modifier,
+        containerColor = Color.LightGray,
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        screens.forEach { screen ->
+
+            NavigationBarItem(
+                label = {
+                    Text(text = screen.titleRes.toString())
+                },
+                icon = {
+                    Icon(imageVector = screen.icon!!, contentDescription = "")
+                },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    unselectedTextColor = Color.Gray, selectedTextColor = Color.White
+                ),
+            )
+        }
+    }
+
+}
