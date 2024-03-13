161f5277818c3881ccc1d396369cb5785b789945
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-03-13 14:27:35 +0100

    Add docstring to data classes

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/placeinfo/PlaceDetails.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/placeinfo/PlaceDetails.kt
@@ -1,5 +1,11 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo
 
+/**
+ * Data class used for retrieving information about each specific place
+ *
+ * Contains the name and description for a specific place. A place in our app context is a spot to take
+ * photos from.
+ */
 data class PlaceDetails(
     val name: String,
     val description: String
