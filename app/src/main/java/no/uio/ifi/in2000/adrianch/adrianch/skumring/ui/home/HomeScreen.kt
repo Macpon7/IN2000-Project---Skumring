3ec659888d3af977d9d2ae3807a1d155f89685fd
marisnot <marisnot@ulrik.uio.no>
2024-03-14 11:54:22 +0100

    Add comments

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/home/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/home/HomeScreen.kt
@@ -19,8 +19,8 @@ import no.uio.ifi.in2000.adrianch.adrianch.skumring.SkumringTopAppBar
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.navigation.NavigationDestination
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.theme.SkumringTheme
 
-object HomeDestination : NavigationDestination {
-    override val icon = Icons.Outlined.Home
+object HomeDestination : NavigationDestination {//This one is used in the SkumringsButtonBar to choose destination
+    override val icon = Icons.Outlined.Home //Show home-icon
     override val buttonTitle = R.string.nav_home_button //This is in Int, have to use stringResource to get the String from string.xml
     override val route = "home"
     override val titleRes = R.string.app_name
