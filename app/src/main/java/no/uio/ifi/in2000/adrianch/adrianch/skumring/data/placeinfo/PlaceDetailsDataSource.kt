279ab07580db625fc8b7aa92b03d57f1e0305df4
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-03-16 16:29:26 +0100

    Separate list of details for preset places into its own file

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/placeinfo/PlaceDetailsDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/placeinfo/PlaceDetailsDataSource.kt
@@ -6,28 +6,10 @@ class PlaceDetailsDataSource {
     //TODO: Implement a real database, not just read from preset list
 
     suspend fun fetchPlaceDetails(id: Int): PlaceDetails {
-        return presetPlaces.find { it.id == id } ?: PlaceDetails(
+        return presetPlacesDetails.find { it.id == id } ?: PlaceDetails(
             id = 0,
             name = "",
             description = ""
         )
     }
 }
-
-val presetPlaces: List<PlaceDetails> = listOf(
-    PlaceDetails(
-        id = 1,
-        name = "Holmenkollen",
-        description = "Lorem Ipsum Dolor Sit Amet"
-    ),
-    PlaceDetails(
-        id = 2,
-        name = "Hellerudtoppen",
-        description = "Lorem Ipsum Dolor Sit Amet"
-    ),
-    PlaceDetails(
-        id = 3,
-        name = "Huk",
-        description = "Lorem Ipsum Dolor Sit Amet"
-    )
-)
