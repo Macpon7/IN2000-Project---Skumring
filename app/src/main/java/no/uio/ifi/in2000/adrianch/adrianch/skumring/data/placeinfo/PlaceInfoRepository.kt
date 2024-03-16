88cf9312167c837be63b15a4d080f59ce5e5ac25
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-03-16 16:00:37 +0100

    Add filtering of WeatherPerHour around time of sunset

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/placeinfo/PlaceInfoRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/placeinfo/PlaceInfoRepository.kt
@@ -8,6 +8,7 @@ import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo.PlaceInfo
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo.SunEvent
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.sunrise.SunActivity
 import java.time.LocalDate
+import kotlin.math.abs
 
 interface PlaceInfoRepository {
     suspend fun getPlaceInfo(lat: String, long: String, id: Int = 0): PlaceInfo
@@ -71,9 +72,19 @@ class PlaceInfoRepositoryImpl (
             )
 
             // Filters away all the WeatherPerHour objects whose time is too far from sunset
-            val sunsetWeather = it.value.filter {
-                //TODO: return true in here if the time of the WeatherPerHour object is the same as sunset +- 1 hour
-                true
+            var sunsetWeather: List<WeatherPerHour> = emptyList()
+            if (it.value.size == 24) {
+                sunsetWeather = it.value.filter { forecastObject ->
+                    abs(sunActivity.sunset.hour - forecastObject.time.hour) < 2
+                }
+            } else {
+                var closestForecast: WeatherPerHour = it.value[0]
+                it.value.forEach { forecastObject ->
+                    if (abs(sunActivity.sunset.hour - forecastObject.time.hour) < abs(sunActivity.sunset.hour - closestForecast.time.hour)) {
+                        closestForecast = forecastObject
+                    }
+                }
+                sunsetWeather = listOf<WeatherPerHour>(closestForecast)
             }
 
             val sunriseWeather = it.value.filter {
