419904f8a221ff83d22a0e5b6caf772d838ca798
marisnot <marisnot@ulrik.uio.no>
2024-03-17 16:56:31 +0100

    Add navController to InfoScreen.kt

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/infoscreen/InfoScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/infoscreen/InfoScreen.kt
@@ -19,10 +19,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
 
 /*
 object InfoScreenDestination : NavigationDestination {
@@ -117,10 +115,4 @@ fun PlacePicture() {
             modifier = Modifier.align(Alignment.Center)
         )
     }
-}
-
-@Composable
-@Preview
-fun Test() {
-    InfoScreen()
 }
