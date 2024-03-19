48180182ff0f0947d3dbfa14ef852e1e7b7ba71d
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-03-19 20:12:03 +0100

    Create PlaceListRepository and a data class for it to output

---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/placeinfo/PlaceSummary.kt
@@ -0,0 +1,9 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo
+
+data class PlaceSummary(
+    val id: Int,
+    val lat: String,
+    val long: String,
+    val name: String,
+    val description: String
+)
