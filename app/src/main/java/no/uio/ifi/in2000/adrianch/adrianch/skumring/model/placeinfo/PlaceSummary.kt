ca318443ac488f83f344b9c54443737f9831cb05
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-03-19 20:27:43 +0100

    Add docstring to data class

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/placeinfo/PlaceSummary.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/placeinfo/PlaceSummary.kt
@@ -1,5 +1,9 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo
 
+/**
+ * Data class containing everything we need to make a list of clickable objects that display
+ * a name and description, and also redirect to the proper PlaceInfoScreen
+ */
 data class PlaceSummary(
     val id: Int,
     val lat: String,
