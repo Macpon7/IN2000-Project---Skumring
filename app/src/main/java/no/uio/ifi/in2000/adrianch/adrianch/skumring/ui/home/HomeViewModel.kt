956209ce5676b987e051a403ab3f10d97366b896
synnovnh <synnovnh@ulrik.uio.no>
2024-03-19 23:36:25 +0100

    updates UiState variables

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/home/HomeViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/home/HomeViewModel.kt
@@ -8,8 +8,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.mapboxpins.MapRepositoryImpl
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.placeinfo.PlaceInfoRepository
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.placeinfo.PlaceInfoRepositoryImpl
+import java.time.LocalDate
 
 data class HomeUiState(
+    var date: LocalDate = LocalDate.of(2024,3,7),
     var time: String = "18:30",
     var temp: String = "25",
     var sunset: String = "19:00",
@@ -17,34 +22,27 @@ data class HomeUiState(
     var weatherMessage: String = "Dårlig vær"
 )
 
-private const val logTag = "HomeViewModel"
+private const val logTag = "HomeViewModel" //for logging
 
 /**
  * ViewModel for HomeScreen
  */
 class HomeViewModel() : ViewModel() {
+    private val mapRepository = MapRepositoryImpl()
+    private val placeInfo: PlaceInfoRepository = PlaceInfoRepositoryImpl()
+
     //repository
     private val _homeUiState = MutableStateFlow(HomeUiState())
     val homeUiState: StateFlow<HomeUiState> = _homeUiState.asStateFlow()
 
     init {
-        //loadHomeScreen()
+        loadHomeScreen()
     }
 
     fun loadHomeScreen(){
         viewModelScope.launch(Dispatchers.IO){
-            //call function that updates time, temp, sunset, weatherMessage
-            //the results will be used in functions in Homescreen
 
-            //SunTempAndTime(){
-            //    text = "Tid: 12:00\nTemperatur: 25°C",
-            //}
-
-            //SunDown(){
-            //    text = "Solnedgang 18:30",
-           // }
-
-            //weatherCheck
-                    }
+        }
     }
 }
+
