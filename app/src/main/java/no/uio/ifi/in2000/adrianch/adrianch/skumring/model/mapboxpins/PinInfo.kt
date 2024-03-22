9c4c755945f6ef546a2f66b64904085dd811d05c
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-03-22 17:10:43 +0100

    Improve docstrings

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/mapboxpins/PinInfo.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/mapboxpins/PinInfo.kt
@@ -1,5 +1,11 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.mapboxpins
 
+/**
+ * Contains and id and coordinated for a pin on the map
+ * @property id Unique integer ID for each place
+ * @property lat Latitude coordinate of this place
+ * @property long Longitude coordinate of this place
+ */
 data class PinInfo(
     val id: Int,
     val lat: String,
--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/placeinfo/PlaceDetails.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/placeinfo/PlaceDetails.kt
@@ -1,10 +1,12 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo
 
 /**
- * Data class used for retrieving information about each specific place
- *
  * Contains the name and description for a specific place. A place in our app context is a spot to take
  * photos from.
+ *
+ * @property id Unique integer id for this place
+ * @property name The name of this place that we display
+ * @property description A longer string with a description of this place
  */
 data class PlaceDetails(
     val id: Int,
