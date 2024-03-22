89a9a32ef62c453c4c68c11e0a9cefa21cd48dd8
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-03-22 17:09:15 +0100

    Improve docstring

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/placeinfo/PlaceListRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/placeinfo/PlaceListRepository.kt
@@ -1,22 +1,30 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.data.placeinfo
 
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.mapboxpins.MapPinsDataSource
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.mapboxpins.PinInfo
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo.PlaceDetails
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.placeinfo.PlaceSummary
 
 interface PlaceListRepository {
-    suspend fun getPlaceList(): List<PlaceSummary>
+    /**
+     * Returns the full list of preset places for displaying in a list format. Combines data from two
+     * sources, [PlaceDetailsDataSource] and [MapPinsDataSource].
+     */
+    suspend fun getPresetPlaceList(): List<PlaceSummary>
 }
 
+/**
+ * Implementation of [PlaceListRepository], used for getting a list of places to display.
+ *
+ * @property placeDetailsDataSource an instance of [PlaceDetailsDataSource]. If none is given, a new instance is created.
+ * @property mapPinsDataSource an instance of [MapPinsDataSource]. If none is given, a new instance is created.
+ */
 class PlaceListRepositoryImpl(
     private val placeDetailsDataSource: PlaceDetailsDataSource = PlaceDetailsDataSource(),
     private val mapPinsDataSource: MapPinsDataSource = MapPinsDataSource()
 ): PlaceListRepository {
-    /**
-     * Combines the two different sources of info for pins and place details, so that our MapListScreen
-     * can display a list of places with name and description, while also being able to send the coordinates
-     * and id of each place as arguments when the user clicks on a list item and goes to a PlaceInfoScreen
-     */
-    override suspend fun getPlaceList(): List<PlaceSummary> {
+
+    override suspend fun getPresetPlaceList(): List<PlaceSummary> {
         val listOfPins = mapPinsDataSource.fetchMapPins()
         val outList = mutableListOf<PlaceSummary>()
 
