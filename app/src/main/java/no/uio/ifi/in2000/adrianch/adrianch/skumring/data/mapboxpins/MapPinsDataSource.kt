8caa9a1bd59b54dca8256cc0a10fd7f8ecab75da
marisnot <marisnot@ulrik.uio.no>
2024-03-24 22:13:53 +0100

    add error-handling for fetchMapPin in MapPinsDataSource.kt

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/mapboxpins/MapPinsDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/mapboxpins/MapPinsDataSource.kt
@@ -1,9 +1,19 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.data.mapboxpins
 
+import android.util.Log
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.mapboxpins.PinInfo
 
 class MapPinsDataSource {
+
+    // Constant for logging errors:
+    private val logTag : String = ""
+
     suspend fun fetchMapPins(): List<PinInfo> {
-        return presetPinsInfo
+        try {
+            return presetPinsInfo
+        } catch (e : Exception) {
+            Log.e(logTag, e.message, e)
+            throw e
+        }
     }
 }
