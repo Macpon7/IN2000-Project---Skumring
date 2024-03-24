ed797440ff7575e98e4159bcabecd3896eb8ca79
annhansg <annhansg@ulrik.uio.no>
2024-03-24 20:18:35 +0100

    add try/catch and log to MapRepository

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/mapboxpins/MapRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/mapboxpins/MapRepository.kt
@@ -1,18 +1,24 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.data.mapboxpins
 
+import android.util.Log
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.mapboxpins.PinInfo
+
 
 interface MapRepository {
     suspend fun getPins(): List<PinInfo>
 }
 
+
 class MapRepositoryImpl(
     private val mapPinsDataSource: MapPinsDataSource = MapPinsDataSource()
-
 ) : MapRepository {
 
     override suspend fun getPins(): List<PinInfo> {
-
-        return mapPinsDataSource.fetchMapPins()
+        try {
+            return mapPinsDataSource.fetchMapPins()
+        } catch (e: Exception) {
+            e.message?.let { Log.e( "MapRepo", it) }
+            throw(e)
+        }
     }
 }
