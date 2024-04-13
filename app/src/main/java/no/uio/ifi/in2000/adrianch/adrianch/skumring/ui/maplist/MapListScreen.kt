a184894abc45a66eecfed4a30e9b1c9aedfe58ec
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-04-13 11:21:33 +0200

    Change ListCard to closer represent final design and make it dynamic

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/maplist/MapListScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/maplist/MapListScreen.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,11 +26,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.outlined.Place
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -317,45 +322,116 @@ fun MapArea(mapListUiState: MapListUiState, navController: NavController) {
  */
 @Composable
 fun ListCard(name: String, description: String, onItemClick: () -> Unit) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 12.dp)
-            .clickable(onClick = onItemClick) //Click to infoscreen
-    ){
+    BoxWithConstraints {
+        if (maxWidth < 400.dp) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+                    .clickable(onClick = onItemClick), //Click to infoscreen
+            ){
 
-        //Box for picture:
-        Box(
-            modifier = Modifier
-                .height(150.dp)
-                .padding(6.dp)
-                .background(Color.LightGray, RoundedCornerShape((16.dp)))
-                .fillMaxWidth(),
-        ) {
-            Text(
-                text = "Image Placeholder",
-                modifier = Modifier.align(Alignment.Center)
-            )
+                //Box for picture:
+                Box(
+                    modifier = Modifier
+                        .height(150.dp)
+                        .background(Color.LightGray, RoundedCornerShape((0.dp)))
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Image Placeholder",
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+
+                Row (
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 8.dp, end = 8.dp)
+                )
+                {
+                    Text(
+                        text = name,
+                        modifier = Modifier
+                            .padding(vertical = 2.dp),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                    Row {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Filled.FavoriteBorder, contentDescription = "")
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Filled.Notifications, contentDescription = "")
+                        }
+                    }
+                }
+
+                //Text for description. Do we want weather condition in the future?
+                Text(
+                    text = description,
+                    modifier = Modifier
+                        .padding(vertical = 2.dp)
+                        .padding(bottom = 4.dp)
+                        .align(Alignment.CenterHorizontally))
+            }
+        } else {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+                    .clickable(onClick = onItemClick), //Click to infoscreen
+            ){
+
+                //Box for picture:
+                Box(
+                    modifier = Modifier
+                        .height(150.dp)
+                        .background(Color.LightGray, RoundedCornerShape((0.dp)))
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Image Placeholder",
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+
+                Row (
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 8.dp, end = 8.dp)
+                )
+                {
+                    Text(
+                        text = name,
+                        modifier = Modifier
+                            .padding(vertical = 2.dp),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Row {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Filled.FavoriteBorder, contentDescription = "")
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Filled.Notifications, contentDescription = "")
+                        }
+                    }
+                }
+
+                //Text for description. Do we want weather condition in the future?
+                Text(
+                    text = description,
+                    modifier = Modifier
+                        .padding(vertical = 2.dp)
+                        .padding(bottom = 4.dp)
+                        .align(Alignment.CenterHorizontally))
+            }
         }
-
-        //Text for name of place
-        Text(
-            text = name,
-            modifier = Modifier
-                .padding(vertical = 2.dp)
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
-        )
-
-        //Text for description. Do we want weather condition in the future?
-        Text(
-            text = description,
-            modifier = Modifier
-                .padding(vertical = 2.dp)
-                .padding(bottom = 4.dp)
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center,)
     }
 }
