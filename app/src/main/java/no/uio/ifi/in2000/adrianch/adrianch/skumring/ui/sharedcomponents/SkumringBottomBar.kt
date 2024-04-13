af013086c64b7bf7953889660a53eb7245008313
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-04-13 13:04:17 +0200

    Fix top and bottom bar as shared components

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/SkumringBottomBar.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/SkumringBottomBar.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,8 +21,7 @@ import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.placeinfo.PlaceInfoScreen
 @Composable
 fun SkumringBottomBar(
     navController: NavHostController,
-    modifier: Modifier = Modifier,
-    state: MutableState<Boolean> // Should show what screen you are on
+    modifier: Modifier = Modifier
 ) {
     // List with the screens, will be updated when we have more screens:
     val screens = listOf(
