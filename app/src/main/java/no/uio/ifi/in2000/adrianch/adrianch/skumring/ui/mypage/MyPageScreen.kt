56ee7d17f4b8ca8e4940a9bc3092489ee3523dbe
marisnot <marisnot@ulrik.uio.no>
2024-04-17 10:44:15 +0200

    add content for NewPlaceDialog and fix variables

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/mypage/MyPageScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/mypage/MyPageScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,7 +16,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -26,10 +28,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -39,6 +45,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -59,7 +66,7 @@ object MyPageDestination : NavigationDestination {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyPageScreen(navController: NavHostController, myPageViewModel: MyPageViewModel = viewModel()) {
-    val myPageUiState: MyPageUiState by myPageViewModel.myListUiState.collectAsState()
+    val myPageUiState: MyPageUiState by myPageViewModel.myPageUiState.collectAsState()
 
     // Check if there is an error, if so show a snackbar:
     if (myPageUiState.showSnackbar) {
@@ -90,12 +97,31 @@ fun MyPageScreen(navController: NavHostController, myPageViewModel: MyPageViewMo
             SkumringTopBar(
                 title = stringResource(id = HomeDestination.titleRes),
                 canNavigateBack = false,
+                actions = {
+                    IconButton(
+                        onClick = {
+                                  //TODO, navigate to settingsScreen
+                            },
+                        modifier = Modifier.padding(end = 16.dp)
+                    ) {
+                        Icon(Icons.Default.Settings, contentDescription = "Settings")
+                    }
+                }
             )
         },bottomBar = {
             SkumringBottomBar(navController = navController)
         },
         snackbarHost = { SnackbarHost(hostState = myPageUiState.snackbarHostState) },
-        ) { innerPadding -> //Here is what will be shown inside the scaffold of the screen
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.padding(end = 16.dp)
+            ) {
+                Icon(Icons.Filled.Add, "Add location")
+            }
+        }
+        )
+    { innerPadding -> //Here is what will be shown inside the scaffold of the screen
         Column (modifier = Modifier.padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
@@ -104,76 +130,126 @@ fun MyPageScreen(navController: NavHostController, myPageViewModel: MyPageViewMo
     }
 }
 
-@Composable
-fun ContentMyPage(navController : NavController, myPageViewModel: MyPageViewModel)
-{
-
-    val myPageUiState: MyPageUiState by myPageViewModel.myListUiState.collectAsState()
-
-    Column {
-        SettingsButton{
-            // The action that happens when the button is pressed happens here:
-
-            // Get send to the SettingsButton when the button is pressed
-
-        }
-
-        var hasLocations = false
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        if (hasLocations) {
-            // Column for list view
-            Column (Modifier.verticalScroll(rememberScrollState())) {
-                myPageUiState.places.forEach {place ->
-                    LocationCard(
-                        name = place.name,
-                        description = place.description,
-                        onItemClick = { //Navigate when it is clicked on. This needs to send lat, long, id
-                            navController.navigate("placeinfoscreen/${place.lat}/${place.long}/${place.id}")
-                        }
-                    )
-                }
-            }
-        } else {
-            Text(
-                text = R.string.no_location.toString(),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-
-        Spacer(modifier = Modifier.weight(1f))
-
-
-        AddLocationButton {
-            // The action that happens when the button is pressed happens here:
-            hasLocations = true //Show the list of locations
-        }
-
-        // TODO: Make as a dialogs
-        if (hasLocations) {
-            Text(text = R.string.new_location.toString())
-        }
-    }
-}
-
 /**
- * Button to go into user-settings
+ * Content in mypage:
+ * SettingsButton
+ * LocationCard
+ * AddLocationButton
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsButton(onClick: () -> Unit){
-    Row(
-        horizontalArrangement = Arrangement.End,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        // Push the icon to the right:
-        Spacer(modifier = Modifier.weight(1f))
-        IconButton(
-            onClick = { onClick() },
-            modifier = Modifier.padding(end = 16.dp)
+fun ContentMyPage(navController : NavController, myPageViewModel: MyPageViewModel) {
+
+    val myPageUiState: MyPageUiState by myPageViewModel.myPageUiState.collectAsState()
+    val newPlaceUiState : NewPlaceUiState by myPageViewModel.newPlaceUiState.collectAsState()
+
+    if (myPageUiState.showNewLocationCard) { //When the user click AddLocationButton this is shown
+
+        // DONT DO THIS, put it in viewmodel
+        var locationName by remember { mutableStateOf("") }
+        var chosenLocation by remember { mutableStateOf("") }
+        var dateOfPicture by remember { mutableStateOf("") }
+        var notes by remember { mutableStateOf("") }
+
+        if (newPlaceUiState.showDatePicker) {
+            DatePickerDialog(
+
+                onDismissRequest = { /*TODO toggle datePickerError and showDatePicker*/ },
+                confirmButton = { /*TODO*/ },
+                dismissButton = { /*TODO*/ }
+
+            ) {
+                DatePicker(state = newPlaceUiState.datePickerState)
+            }
+        }
+
+
+        // Lag dette som et eget card
+        // Bruk surface of card skal poppe opp, sjekk maplistscreen
+        Card(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
         ) {
-            Icon(Icons.Default.Settings, contentDescription = "Settings")
+            // String with name
+            TextField(
+                value = "",// Take variable from newPlaceUiState
+                onValueChange = { locationName = it },
+                label = { Text("Location") }
+            )
+            // Brukeren skal skrive inn addresse:
+            TextField(
+                value = chosenLocation,
+                onValueChange = { chosenLocation = it },
+                label = { Text("Choose location") }
+            )
+            // Time skal lages made datepicker dialog
+            TextField(
+                value = dateOfPicture,
+                onValueChange = {  },
+                modifier = Modifier.clickable(
+                    enabled = true,
+                    onClick = {
+                        //TODO enable date picker in viewmodel with boolean
+                    }
+                ),
+                enabled = false,
+                isError = newPlaceUiState.datePickerError,
+                label = { Text("Time of picture") }
+            )
+
+            // String with description
+            TextField(
+                value = notes,
+                onValueChange = { notes = it },
+                label = { Text("Notes") }
+            )
+            Button(
+                onClick = {
+                    //TODO
+                } ) {
+                Text("Add a photo")
+            }
+            Button(
+                onClick = { },
+                modifier = Modifier.padding(vertical = 8.dp),
+            ) {
+                Text("Add Location")
+            }
+        }
+    } else {
+        // Need to have column in order to use spacer inside the Column to push the add-icon down on the page
+        Column {
+            var hasLocations = false
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            if (hasLocations) {
+                // Column for list view
+                Column (Modifier.verticalScroll(rememberScrollState())) {
+                    myPageUiState.places.forEach {place ->
+                        LocationCard(
+                            name = place.name,
+                            description = place.description,
+                            onItemClick = { //Navigate when it is clicked on. This needs to send lat, long, id
+                                navController.navigate("placeinfoscreen/${place.lat}/${place.long}/${place.id}")
+                            }
+                        )
+                    }
+                }
+            } else {
+                Text(
+                    text = stringResource(R.string.no_location),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+
+            // TODO: Make as a dialogs
+            if (hasLocations) {
+                Text(text = R.string.new_location.toString())
+            }
         }
     }
 }
@@ -226,29 +302,20 @@ fun LocationCard(name: String, description: String, onItemClick: () -> Unit) {
     }
 }
 
-/**
- * Button for adding your own locations to my page
- * Will show a new "page" that allow you to add the location
- */
 @Composable
-fun AddLocationButton(onClick: () -> Unit) {
-    Row(
-        horizontalArrangement = Arrangement.End,
-        modifier = Modifier.padding(16.dp)
-    ) {
-        // Push the icon to the right:
-        Spacer(modifier = Modifier.weight(1f))
-        FloatingActionButton(
-            onClick = { onClick() },
-            modifier = Modifier.padding(end = 16.dp)
-        ) {
-            Icon(Icons.Filled.Add, "Add location")
-        }
+fun NewPlaceDialog() {
+    Dialog(onDismissRequest = { /*TODO*/ }) {
+        
     }
 }
 
 @Composable
 @Preview
-fun Test(navController: NavHostController = rememberNavController()) {
-    MyPageScreen(navController = navController)
+fun TestList(navController: NavHostController = rememberNavController()) {
+    MyPageScreen(
+        navController = navController,
+        myPageViewModel = MyPageViewModel(
+
+        )
+        )
 }
