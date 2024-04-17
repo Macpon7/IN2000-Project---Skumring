c76e10f3cef5649292c631bf661240376aea6108
marisnot <marisnot@ulrik.uio.no>
2024-04-17 10:42:32 +0200

    fix variables in MyPageViewModel.kt, add new dataclass NewPlaceUiState

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/mypage/MyPageViewModel.kt
@@ -1,6 +1,9 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.mypage
 
-import android.util.Log
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SnackbarHostState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -17,12 +20,36 @@ data class MyPageUiState(
     val pins: List<PinInfo> = emptyList(),
     val places: List<PlaceSummary> = emptyList(),
 
+    var showNewLocationCard : Boolean = true,
     // Variable for checking if there is an error:
     var showSnackbar: Boolean = false,
     // Variable that change according to the error message we get:
     var errorMessage: String = "No error",
     // Variable for snackbar:
     val snackbarHostState: SnackbarHostState = SnackbarHostState()
+)
+
+data class NewPlaceUiState @OptIn(ExperimentalMaterial3Api::class) constructor(
+    var locationName : String = "",
+    // TODO add location
+    // TODO get date from user
+    // val dateOfImage: LocalDate,
+    var descriptions : String = "",
+
+    var datePickerState: DatePickerState = DatePickerState(
+        initialSelectedDateMillis = null,
+        initialDisplayedMonthMillis = null,
+        yearRange = DatePickerDefaults.YearRange,
+        initialDisplayMode = DisplayMode.Picker
+        ),
+
+
+    // Show the date picker when the user want to pick a date
+    var showDatePicker : Boolean = true,
+
+    // Show an error if the use did not pick a date
+    var datePickerError : Boolean = false
+
 )
 
 private const val logTag = "MyPageViewModel"
@@ -33,37 +60,50 @@ class MyPageViewModel : ViewModel() {
     private val placeListRepository = PlaceListRepositoryImpl()
 
 
-    private val _mapListUiState = MutableStateFlow(MyPageUiState())
-    val myListUiState: StateFlow<MyPageUiState> = _mapListUiState
+    private val _myPageUiState = MutableStateFlow(MyPageUiState())
+    val myPageUiState: StateFlow<MyPageUiState> = _myPageUiState
+
+    private val _newPlaceUiState = MutableStateFlow(NewPlaceUiState())
+    val newPlaceUiState : StateFlow<NewPlaceUiState> = _newPlaceUiState
 
     init {
         loadList()
+    }
+
+    fun updateNewLocationName() {
+
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun showNewForm() {
+        viewModelScope.launch (Dispatchers.IO) {
+            _myPageUiState.update { currentMyPageUiState ->
+                currentMyPageUiState.copy(showNewLocationCard = true)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun hideNewForm() {
+        viewModelScope.launch (Dispatchers.IO) {
+            _myPageUiState.update {currentMyPageUiState ->
+                currentMyPageUiState.copy(showNewLocationCard = false)
+            }
+        }
     }
 
     /**
      * Load the list of places
      */
     private fun loadList(){
-        viewModelScope.launch(Dispatchers.IO) {
-            _mapListUiState.update { currentMapListUiState ->
-                try {
-                    // TODO: This will be changed with the repository of the places the user saves
-                    val placeSummaryList = placeListRepository.getPresetPlaceList()
-                    currentMapListUiState.copy(places = placeSummaryList)
-                } catch(e: Exception) {
-                    Log.e(logTag, "Error getting pins in loadList", e)
-                    currentMapListUiState.copy(showSnackbar = true,
-                        errorMessage = "Error getting pins in loadList")
-                }
-            }
-        }
+        // TODO load the list of the users customs places, will probably look similar to the list in maplistviewmodel
     }
 
     /**
      * showSnackbar is set to false and the snackbar disappear
      */
     fun snackbarDismissed() {
-        _mapListUiState.update { currentMapUiState ->
+        _myPageUiState.update { currentMapUiState ->
             currentMapUiState.copy(showSnackbar = false)
         }
     }
@@ -72,7 +112,7 @@ class MyPageViewModel : ViewModel() {
      *    For refreshing when you use snackbar in MyPageScreen:
      */
     fun refresh() {
-        _mapListUiState.update {currentMapUiState ->
+        _myPageUiState.update { currentMapUiState ->
             currentMapUiState.copy(showSnackbar = false) }
         viewModelScope.launch (Dispatchers.IO) {
             loadList()
