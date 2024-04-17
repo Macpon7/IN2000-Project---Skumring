aa5bc01580d3b9a21f9367e406230a419c594b7d
viljardh <viljardh@ifi.uio.no>
2024-04-17 19:18:47 +0200

    add datafiles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -142,5 +142,7 @@ dependencies {
 
     // Location
     implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.31.3-beta")
+
 
 }
---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/userlocation/UserLocationDataSource.kt
@@ -0,0 +1,11 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.userlocation
+
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+
+class UserLocationDataSource {
+    private val logTag: String = "UserLocationDataSource"
+
+
+}
