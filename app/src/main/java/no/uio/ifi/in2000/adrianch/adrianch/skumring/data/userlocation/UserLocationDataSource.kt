ee25500a7031540bccd80e782d5654ce1e0cb27e
viljardh <viljardh@ifi.uio.no>
2024-04-19 15:47:37 +0200

    moved to data layer

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/userlocation/UserLocationDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/userlocation/UserLocationDataSource.kt
@@ -1,17 +1,15 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.data.userlocation
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.location.Location
 import android.util.Log
-import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.userlocation.UserLocation
 
-class UserLocation (context: Context) {
-    private val logTag = "UserLocation"
+class UserLocationDataSource (context: Context) {
+    private val logTag = "UserLocationDataSource"
     private var fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
     private var long = "10.718393"
@@ -19,7 +17,7 @@ class UserLocation (context: Context) {
 
     // TODO check for permissions lol
     @SuppressLint("MissingPermission")
-    private fun getLastLocation(fusedLocationClient: FusedLocationProviderClient, context: Context) {
+    private fun getLastLocation(fusedLocationClient: FusedLocationProviderClient): UserLocation {
         Log.d(logTag, "Trying to fetch loc")
 
         fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
@@ -29,5 +27,7 @@ class UserLocation (context: Context) {
                 Log.d(logTag, "Long: $long Lat: $lat")
             }
         }
+
+        return UserLocation(long = long, lat = lat)
     }
 }
