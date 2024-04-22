6114c6dc5274fda9dffa7f545f8c2e525a703b77
annhansg <annhansg@ulrik.uio.no>
2024-04-22 21:31:36 +0200

    fix light colortheme for screens

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/theme/Color.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/theme/Color.kt
@@ -12,67 +12,125 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
  */
 
-
-val md_theme_light_primary = Color(0xFF6D47B8)
+/*
+//Light colors
+val md_theme_light_primary = Color(0xFF8034C9)
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
-val md_theme_light_primaryContainer = Color(0xFFEADDFF)
-val md_theme_light_onPrimaryContainer = Color(0xFF25005A)
-val md_theme_light_secondary = Color(0xFF7742B6)
+val md_theme_light_primaryContainer = Color(0xFFF0DBFF)
+val md_theme_light_onPrimaryContainer = Color(0xFF2C0051)
+val md_theme_light_secondary = Color(0xFF6644CB) //toggle button
 val md_theme_light_onSecondary = Color(0xFFFFFFFF)
-val md_theme_light_secondaryContainer = Color(0xFFEEDBFF)
-val md_theme_light_onSecondaryContainer = Color(0xFF2A0053)
-val md_theme_light_tertiary = Color(0xFF6A4FA3)
+val md_theme_light_secondaryContainer = Color(0xFF392181) //card button color & changing weathercondition color
+val md_theme_light_onSecondaryContainer = Color(0xFFFFFFFF) //card buttons & text color
+val md_theme_light_tertiary = Color(0xFF774A9B)
 val md_theme_light_onTertiary = Color(0xFFFFFFFF)
-val md_theme_light_tertiaryContainer = Color(0xFFEADDFF)
-val md_theme_light_onTertiaryContainer = Color(0xFF24005A)
+val md_theme_light_tertiaryContainer = Color(0xFFF2DAFF) //background toggle button
+val md_theme_light_onTertiaryContainer = Color(0xFF2E004E)
 val md_theme_light_error = Color(0xFFBA1A1A)
 val md_theme_light_errorContainer = Color(0xFFFFDAD6)
 val md_theme_light_onError = Color(0xFFFFFFFF)
 val md_theme_light_onErrorContainer = Color(0xFF410002)
-val md_theme_light_background = Color(0xFFFFFBFF)
-val md_theme_light_onBackground = Color(0xFF1D1B1E)
-val md_theme_light_surface = Color(0xFFFFFBFF)
-val md_theme_light_onSurface = Color(0xFF1D1B1E)
-val md_theme_light_surfaceVariant = Color(0xFFE7E0EB)
-val md_theme_light_onSurfaceVariant = Color(0xFF49454E)
-val md_theme_light_outline = Color(0xFF7A757F)
-val md_theme_light_inverseOnSurface = Color(0xFFF5EFF4)
-val md_theme_light_inverseSurface = Color(0xFF323033)
-val md_theme_light_inversePrimary = Color(0xFFD2BBFF)
+val md_theme_light_background = Color(0xFFB3AFB8)
+val md_theme_light_onBackground = Color(0xFF001B3D)
+val md_theme_light_surface = Color(0xFFFDFBFF)
+val md_theme_light_onSurface = Color(0xFF001B3D)
+val md_theme_light_surfaceVariant = Color(0xFFE9DFEB)
+val md_theme_light_onSurfaceVariant = Color(0xFF4A454E)
+val md_theme_light_outline = Color(0xFF7C757E)
+val md_theme_light_inverseOnSurface = Color(0xFF4A454E) //bottombar text color
+val md_theme_light_inverseSurface = Color(0xFF003062)
+val md_theme_light_inversePrimary = Color(0xFFDCB8FF)
 val md_theme_light_shadow = Color(0xFF000000)
-val md_theme_light_surfaceTint = Color(0xFF6D47B8)
-val md_theme_light_outlineVariant = Color(0xFFCBC4CF)
-val md_theme_light_scrim = Color(0xFF000000)
 
-val md_theme_dark_primary = Color(0xFFD2BBFF)
-val md_theme_dark_onPrimary = Color(0xFF3E0A87)
-val md_theme_dark_primaryContainer = Color(0xFF552C9E)
-val md_theme_dark_onPrimaryContainer = Color(0xFFEADDFF)
-val md_theme_dark_secondary = Color(0xFFDAB9FF)
-val md_theme_dark_onSecondary = Color(0xFF460084)
-val md_theme_dark_secondaryContainer = Color(0xFF5E279C)
-val md_theme_dark_onSecondaryContainer = Color(0xFFEEDBFF)
-val md_theme_dark_tertiary = Color(0xFFD2BCFF)
-val md_theme_dark_onTertiary = Color(0xFF3A1D71)
-val md_theme_dark_tertiaryContainer = Color(0xFF513689)
-val md_theme_dark_onTertiaryContainer = Color(0xFFEADDFF)
+//SunsetInfoCardColors - light
+val md_theme_light_scrim = Color(0xFF8498C2) //SunsetInfoCard color
+val md_theme_light_surfaceTint = Color(0xFF1593CE) //SunsetInfoCard color
+val md_theme_light_outlineVariant = Color(0xFF0A4BF0) //SunsetInfoCard color & color for divider
+*/
+
+
+val md_theme_light_primary = Color(0xFF0062A1)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFD0E4FF)
+val md_theme_light_onPrimaryContainer = Color(0xFF001D35)
+val md_theme_light_secondary = Color(0xFF006689) //toggle button
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFC3E8FF) //card button color
+val md_theme_light_onSecondaryContainer = Color(0xFF001E2C)////card buttons & text color
+val md_theme_light_tertiary = Color(0xFF014392)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFCCE5FF)
+val md_theme_light_onTertiaryContainer = Color(0xFF001D31)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFDFCFF) //Background of card color
+val md_theme_light_onBackground = Color(0xFF1A1C1E)
+val md_theme_light_surface = Color(0xFFFDFCFF)
+val md_theme_light_onSurface = Color(0xFF1A1C1E)
+val md_theme_light_surfaceVariant = Color(0xFFDFE3EB)
+val md_theme_light_onSurfaceVariant = Color(0xFF42474E)
+val md_theme_light_inverseOnSurface = Color(0xFF2F3033) //bottombar text color
+val md_theme_light_inverseSurface = Color(0xFFF1F0F4)
+val md_theme_light_inversePrimary = Color(0xFF9CCAFF)
+val md_theme_light_shadow = Color(0xFF000000)
+
+
+val md_theme_light_outline = Color(0xFFFFFFFF) //"Sunset today" color
+//Sunset colors
+val md_theme_light_surfaceTint = Color(0xFF0062A1)
+val md_theme_light_outlineVariant = Color(0xFFC3E8FF)
+val md_theme_light_scrim = Color(0xFF082E4B)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Dark colors
+val md_theme_dark_primary = Color(0xFFDCB8FF)
+val md_theme_dark_onPrimary = Color(0xFF490081)
+val md_theme_dark_primaryContainer = Color(0xFF670CB0)
+val md_theme_dark_onPrimaryContainer = Color(0xFFF0DBFF)
+val md_theme_dark_secondary = Color(0xFF4E26B2) //toggle button
+val md_theme_dark_onSecondary = Color(0xFF370096)
+val md_theme_dark_secondaryContainer = Color(0xFF4E26B2) //card button color
+val md_theme_dark_onSecondaryContainer = Color(0xFFE7DEFF) ////card buttons & text color
+val md_theme_dark_tertiary = Color(0xFFE1B6FF)
+val md_theme_dark_onTertiary = Color(0xFF461968)
+val md_theme_dark_tertiaryContainer = Color(0xFFE7DEFF) //background toggle button
+val md_theme_dark_onTertiaryContainer = Color(0xFFF2DAFF)
 val md_theme_dark_error = Color(0xFFFFB4AB)
 val md_theme_dark_errorContainer = Color(0xFF93000A)
 val md_theme_dark_onError = Color(0xFF690005)
 val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
-val md_theme_dark_background = Color(0xFF1D1B1E)
-val md_theme_dark_onBackground = Color(0xFFE6E1E6)
-val md_theme_dark_surface = Color(0xFF1D1B1E)
-val md_theme_dark_onSurface = Color(0xFFE6E1E6)
-val md_theme_dark_surfaceVariant = Color(0xFF49454E)
-val md_theme_dark_onSurfaceVariant = Color(0xFFCBC4CF)
-val md_theme_dark_outline = Color(0xFF948F99)
-val md_theme_dark_inverseOnSurface = Color(0xFF1D1B1E)
-val md_theme_dark_inverseSurface = Color(0xFFE6E1E6)
-val md_theme_dark_inversePrimary = Color(0xFF6D47B8)
-val md_theme_dark_shadow = Color(0xFF000000)
-val md_theme_dark_surfaceTint = Color(0xFFD2BBFF)
-val md_theme_dark_outlineVariant = Color(0xFF49454E)
-val md_theme_dark_scrim = Color(0xFF000000)
+val md_theme_dark_background = Color(0xFF1D1C1D) // Background of card color
+val md_theme_dark_onBackground = Color(0xFFD6E3FF)
+val md_theme_dark_surface = Color(0xFF121212)
+val md_theme_dark_onSurface = Color(0xFFD6E3FF)
+val md_theme_dark_surfaceVariant = Color(0xFF4A454E)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCCC4CE)
 
-val seed = Color(0xFF7751C2)
+val md_theme_dark_inverseSurface = Color(0xFFD6E3FF)
+val md_theme_dark_inversePrimary = Color(0xFF8034C9)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_inverseOnSurface = Color(0xFFE7DEFF) //bottombar text color
+
+
+
+val md_theme_dark_outline = Color(0xFFE7DEFF) //"Sunset today" color
+//SunsetInfoCardColors -dark
+val md_theme_dark_scrim = Color(0xFF1B1B1F) //SunsetInfoCard color
+val md_theme_dark_surfaceTint = Color(0xFF8146A8) //SunsetInfoCard color
+val md_theme_dark_outlineVariant = Color(0xFF2F107C) //SunsetInfoCard color
+
+
+val seed = Color(0xFF7627BF)
