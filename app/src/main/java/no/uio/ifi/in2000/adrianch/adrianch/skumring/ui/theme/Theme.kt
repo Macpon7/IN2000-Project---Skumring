a06c1e4a71e5b60d050c19e42d6d8f7de1a4a762
annhansg <annhansg@ulrik.uio.no>
2024-04-22 21:32:22 +0200

    add light and dark colorthemes for screens

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/theme/Theme.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/theme/Theme.kt
@@ -1,15 +1,24 @@
+@file:Suppress("IMPLICIT_CAST_TO_ANY")
+
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -68,8 +77,12 @@ private val LightColors = lightColorScheme(
     inverseOnSurface = md_theme_light_inverseOnSurface,
     inverseSurface = md_theme_light_inverseSurface,
     inversePrimary = md_theme_light_inversePrimary,
+
+
     surfaceTint = md_theme_light_surfaceTint,
     outlineVariant = md_theme_light_outlineVariant,
+    scrim = md_theme_light_scrim,
+
 )
 
 
@@ -103,10 +116,12 @@ private val DarkColors = darkColorScheme(
     surfaceTint = md_theme_dark_surfaceTint,
     outlineVariant = md_theme_dark_outlineVariant,
     scrim = md_theme_dark_scrim,
+
 )
 
+
 @Composable
-fun AppTheme(
+fun SkumringTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
@@ -122,34 +137,45 @@ fun AppTheme(
     )
 }
 
-@Composable
-fun SkumringTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
-        darkTheme -> DarkColors
-        else -> LightColors
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+@Composable
+fun LightInfoCardScreen(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (!useDarkTheme) {
+        Box( //Need box as an overlay over card for color gradient
+            modifier = Modifier
+                .background(
+                    Brush.verticalGradient(listOf(
+                    MaterialTheme.colorScheme.scrim,// MaterialTheme.colorScheme.onSecondaryContainer,
+                    MaterialTheme.colorScheme.primary,  //surfaceTint
+                    MaterialTheme.colorScheme.outlineVariant,
+                    )
+                )))
+    } else {
+        Box( //Need box as an overlay over card for color gradient
+            modifier = Modifier
+                .background(
+                    Brush.verticalGradient(listOf(
+                        MaterialTheme.colorScheme.scrim,// MaterialTheme.colorScheme.onSecondaryContainer,
+                        MaterialTheme.colorScheme.surfaceTint,
+                        MaterialTheme.colorScheme.outlineVariant,
+
+                        )
+                    )))
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
+
+
+/*
+
+surfaceTint = md_theme_light_surfaceTint,
+outlineVariant = md_theme_light_outlineVariant,
+scrim = md_theme_light_scrim,
+ */
