a6b31334f54af7f72d3a9d417a7734552c7fecf9
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-04-23 11:19:42 +0200

    Refactor: Rename logTag to TAG and autoformat code

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/database/AppDatabase.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/data/database/AppDatabase.kt
@@ -7,9 +7,13 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [PlaceInfoEntity::class, ImageEntity::class, ForecastEntity::class], version = 1)
+private const val TAG = "AppDatabase"
+
+@Database(
+    entities = [PlaceInfoEntity::class, ImageEntity::class, ForecastEntity::class], version = 1
+)
 @TypeConverters(RoomConverters::class)
-public abstract class AppDatabase : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun placeInfoDao(): PlaceInfoDao
     abstract fun imageDao(): ImageDao
     abstract fun forecastDao(): ForecastDao
@@ -21,38 +25,17 @@ public abstract class AppDatabase : RoomDatabase() {
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
-            Log.d("AppDatabase","Initializes database")
+            Log.d(TAG, "Initializes database")
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "placeinfo_database"
+                    context.applicationContext, AppDatabase::class.java, "placeinfo_database"
                 ).build()
                 INSTANCE = instance
                 // return instance
                 instance
             }
         }
-    }}
-
-//Old method
-/*
-    fun buildDatabase(context: Context) : AppDatabase{
-        return Room.databaseBuilder(context, AppDatabase::class.java, "placeInfo.db").build()
     }
-
-
-    @Volatile
-    private var INSTANCE : AppDatabase? = null
-
-    fun getDatabase(context: Context): AppDatabase{
-        if (INSTANCE == null){
-            synchronized(this){
-            INSTANCE = buildDatabase(context)}
-        }
-        return INSTANCE!!
-    }
-
-        */
+}
