53791b35acab20ce92bc6af6aea4abed9701abaa
marisnot <marisnot@ulrik.uio.no>
2024-04-24 11:36:50 +0200

    add enum for SettingsScreen.kt

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/settings/SettingsOptions.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/settings/SettingsOptions.kt
@@ -4,3 +4,25 @@ package no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.settings
 
 // Lag det som alpaca
 
+enum class Theme {
+    FOLLOW_SYSTEM,
+    DARK_MODE,
+    LIGHT_MODE
+}
+
+enum class Language {
+    FOLLOW_SYSTEM,
+    NORWEGIAN,
+    ENGLISH
+}
+
+enum class Location {
+    COSTUME_LOCATION,
+    PHONES_LOCATION
+}
+
+enum class Show_Location_as {
+    DRIVE,
+    BIKE,
+    WALK
+}
