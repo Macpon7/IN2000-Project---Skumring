0a52691c9a501d2bffb9e80eda8266ad2a9e4c10
marisnot <marisnot@ulrik.uio.no>
2024-04-25 15:33:59 +0200

    fix how the variables of SettingsUiState get updated in SettingsViewModel.kt

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/settings/SettingsViewModel.kt
@@ -1,14 +1,20 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.settings
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.compose.material3.SnackbarHostState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.ApplicationSkumring
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.R
 
 /**
  * Ui state for settings
@@ -21,22 +27,22 @@ data class SettingsUiState(
 
     // Variables for choosing theme:
     var theme: Theme = Theme.FOLLOW_SYSTEM,
-    var selectedDropDownOptionTheme : String = "Follow system",
+    var selectedDropDownOptionTheme : String = "",
     var dropdownExpandedTheme : Boolean = false,
 
     // Variables for choosing language:
     var language: Language = Language.FOLLOW_SYSTEM,
-    var selectedDropDownOptionLanguage: String = "Follow system",
+    var selectedDropDownOptionLanguage: String = "",
     var dropdownExpandedLanguage: Boolean = false,
 
     // Variables for choosing Location:
     var location: Location = Location.PHONES_LOCATION,
-    var selectedDropDownOptionLocation : String = "Phone's location",
+    var selectedDropDownOptionLocation : String = "",
     var dropdownExpandedStartLocation: Boolean = false,
 
     // variables for choosing show location as
-    var locationAs : Show_Location_as = Show_Location_as.WALK,
-    var selectedLocationAs : String = "Walk",
+    var locationAs : LocationAs = LocationAs.WALK,
+    var selectedLocationAs : String = "",
     var dropdownExpandedLocationAs: Boolean = false,
 
     // Variable for checking if there is an error:
@@ -50,35 +56,68 @@ data class SettingsUiState(
 //Used to log errors in logcat:
 private const val logTag = "SettingsViewModel"
 
-class SettingsViewModel : ViewModel() {
+@SuppressLint("StaticFieldLeak")
+class SettingsViewModel (private val context: Context) : ViewModel() {
 
     private val _settingsUiState = MutableStateFlow(SettingsUiState())
     val settingsUiState: StateFlow<SettingsUiState> = _settingsUiState.asStateFlow()
 
-    // TODO
-    fun updateNotificationEnabled(isChecked : Boolean) {
-        viewModelScope.launch (Dispatchers.IO) {
-            _settingsUiState.update {currentSettingsUiState ->
-                currentSettingsUiState.copy(notificationEnabled = isChecked)
-            }
-        }
+    /**
+     * This function is used first time an instance of SettingsViewModel is made
+     */
+    init {
+        setDisplayStrings()
     }
 
     /**
-     * Function for updating the string selectedMode in
-     * TODO more comments
+     * Set the default variables for variables in the settings
+     * This happens first time the settingsscreen start
      */
-    fun updateTheme(theme : Theme, option : String) {
-        viewModelScope.launch (Dispatchers.IO) {
-            _settingsUiState.update {currentSettingsUiState ->
+    fun setDisplayStrings() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _settingsUiState.update { currentSettingsUiState ->
                 currentSettingsUiState.copy(
-                    theme = theme,
-                    selectedDropDownOptionTheme = option
+                    selectedDropDownOptionTheme = context.resources.getString(R.string.follow_system),
+                    selectedDropDownOptionLanguage = context.resources.getString(R.string.follow_system),
+                    selectedDropDownOptionLocation = context.resources.getString(R.string.phones_location),
+                    selectedLocationAs = context.resources.getString(R.string.walk)
                 )
             }
         }
     }
 
+    // Functions for theme:
+
+    /**
+     * Update variables depending on which dropdownmenu-item the user click:
+     * The string-variable of theme
+     * The enum-theme-variable
+     * Also update the dropdownExpandedTheme to false, which will make the dropdownmenu not visible anymore
+     */
+    fun updateTheme(theme : Theme) {
+        viewModelScope.launch (Dispatchers.IO) {
+            _settingsUiState.update {currentSettingsUiState ->
+                val displayString = when(theme) {
+                    Theme.FOLLOW_SYSTEM -> context.resources.getString(R.string.follow_system)
+                    Theme.DARK_MODE -> context.resources.getString(R.string.dark_mode)
+                    Theme.LIGHT_MODE -> context.resources.getString(R.string.light_mode)
+                }
+                currentSettingsUiState.copy(
+                    theme = theme,
+                    selectedDropDownOptionTheme = displayString,
+                    dropdownExpandedTheme = false
+                )
+            }
+        }
+    }
+
+    /**
+     * Update the dropdownExpandedTheme boolean variable
+     * if the variable is false it will be true
+     * if it is true it will be false
+     * When the variable is false the dropdownmenu won't be visible
+     * When the variable is true the dropdownmenu will be visible
+     */
     fun expandDropdownTheme() {
         viewModelScope.launch (Dispatchers.IO) {
             _settingsUiState.update {currentSettingsUiState ->
@@ -88,17 +127,38 @@ class SettingsViewModel : ViewModel() {
         }
     }
 
-    // TODO comments
-    fun updateLanguage(language : Language, option : String) {
+    // Functions for StartLocation
+
+    /**
+     * Update variables depending on which dropdownmenu-item the user click:
+     * The string-variable of language
+     * The enum-language-variable
+     * Also update the dropdownExpandedLanguage to false, which will make the dropdownmenu not visible anymore
+     */
+    fun updateLanguage(language : Language) {
         viewModelScope.launch (Dispatchers.IO) {
             _settingsUiState.update {currentSettingsUiState ->
+                val displayString = when(language) {
+                    Language.FOLLOW_SYSTEM -> context.resources.getString(R.string.follow_system)
+                    Language.ENGLISH -> context.resources.getString(R.string.english)
+                    Language.NORWEGIAN -> context.resources.getString(R.string.norwegian)
+                }
                 currentSettingsUiState.copy(
                     language = language,
-                    selectedDropDownOptionLanguage = option)
+                    selectedDropDownOptionLanguage = displayString,
+                    dropdownExpandedLanguage = false
+                    )
             }
         }
     }
 
+    /**
+     * Update the dropdownExpandedLanguage boolean variable
+     * if the variable is false it will be true
+     * if it is true it will be false
+     * When the variable is false the dropdownmenu won't be visible
+     * When the variable is true the dropdownmenu will be visible
+     */
     fun expandDropdownLanguage() {
         viewModelScope.launch (Dispatchers.IO) {
             _settingsUiState.update {currentSettingsUiState ->
@@ -108,7 +168,37 @@ class SettingsViewModel : ViewModel() {
         }
     }
 
-    // TODO comments
+    // Functions for StartLocation:
+
+    /**
+     * Update variables depending on which dropdownmenu-item the user click:
+     * The string-variable of defaultlocation
+     * The enum-location-variable
+     * Also update the dropdownExpandedStartLocation to false, which will make the dropdownmenu not visible anymore
+     */
+    fun updateSelectedDefaultLocation(location : Location) {
+        viewModelScope.launch (Dispatchers.IO) {
+            val displayString = when(location) {
+                Location.COSTUM_LOCATION -> context.resources.getString(R.string.costum_location)
+                Location.PHONES_LOCATION -> context.resources.getString(R.string.phones_location)
+            }
+            _settingsUiState.update {currentSettingsUiState ->
+                currentSettingsUiState.copy(
+                    location = location,
+                    selectedDropDownOptionLocation = displayString,
+                    dropdownExpandedStartLocation = false
+                    )
+            }
+        }
+    }
+
+    /**
+     * Update the dropdownExpandedStartLocation boolean variable
+     * if the variable is false it will be true
+     * if it is true it will be false
+     * When the variable is false the dropdownmenu won't be visible
+     * When the variable is true the dropdownmenu will be visible
+     */
     fun expandDropdownStartLocation() {
         viewModelScope.launch (Dispatchers.IO) {
             _settingsUiState.update {currentSettingsUiState ->
@@ -118,44 +208,43 @@ class SettingsViewModel : ViewModel() {
         }
     }
 
+    // Functions for LocationAs:
+
     /**
-     * Function to update selected default location
-     * The function update the variable selectedDefaultLocation which is of the enum-class Location
-     * It also update the variable option which is a string
-     * The variable option is used to save the strings that is shown in the screens
+     * Update variables depending on which dropdownmenu-item the user click:
+     * The string-variable of LocationAs
+     * The enum-LocationAs-variable
+     * Also update the dropdownExpandedLocationAs to false, which will make the dropdownmenu not visible anymore
      */
-    fun updateSelectedDefaultLocation(location : Location, option : String) {
+    fun updateSelectedLocationAs(locationAs: LocationAs) {
         viewModelScope.launch (Dispatchers.IO) {
             _settingsUiState.update {currentSettingsUiState ->
+                val displayString = when (locationAs) {
+                    LocationAs.WALK -> context.resources.getString(R.string.walk)
+                    LocationAs.BIKE -> context.resources.getString(R.string.bike)
+                    LocationAs.DRIVE -> context.resources.getString(R.string.drive)
+                }
                 currentSettingsUiState.copy(
-                    location = location,
-                    selectedDropDownOptionLocation = option)
+                    locationAs = locationAs,
+                    selectedLocationAs = displayString,
+                    dropdownExpandedLocationAs = false
+                )
             }
         }
     }
 
-    // TODO comments
+    /**
+     * Update the dropdownExpandedLocationAs boolean variable
+     * if the variable is false it will be true
+     * if it is true it will be false
+     * When the variable is false the dropdownmenu won't be visible
+     * When the variable is true the dropdownmenu will be visible
+     */
     fun expandDropdownLocationAs() {
         viewModelScope.launch (Dispatchers.IO) {
             _settingsUiState.update {currentSettingsUiState ->
                 currentSettingsUiState.copy(
                     dropdownExpandedLocationAs = !currentSettingsUiState.dropdownExpandedLocationAs)
-            }
-        }
-    }
-
-    /**
-     * Function to update selected default location
-     * The function update the variable selectedDefaultLocation which is of the enum-class Location
-     * It also update the variable option which is a string
-     * The variable option is used to save the strings that is shown in the screens
-     */
-    fun updateSelectedLocationAs(locationAs: Show_Location_as, option : String) {
-        viewModelScope.launch (Dispatchers.IO) {
-            _settingsUiState.update {currentSettingsUiState ->
-                currentSettingsUiState.copy(
-                    locationAs = locationAs,
-                    selectedLocationAs = option)
             }
         }
     }
@@ -180,6 +269,22 @@ class SettingsViewModel : ViewModel() {
         }
         viewModelScope.launch (Dispatchers.IO) {
             //TODO Load again here
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    companion object {
+        val Factory: ViewModelProvider.Factory = object: ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras,
+            ): T {
+                val application = checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]) as ApplicationSkumring
+
+                return SettingsViewModel(
+                    context = application.context
+                ) as T
+            }
         }
     }
 }
