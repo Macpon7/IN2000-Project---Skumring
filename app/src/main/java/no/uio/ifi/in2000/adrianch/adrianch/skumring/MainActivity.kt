46c8b852bae84d336f6d1e20c2554d9132d95ee1
synnovnh <synnovnh@ulrik.uio.no>
2024-04-25 15:18:29 +0200

    clean up the file

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/MainActivity.kt
@@ -1,25 +1,13 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.GeneratedAdapter
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.theme.SkumringTheme
-import java.io.File
-import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +20,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     SkumringApp()
-
-    }}}}
-
-
-
-
-
-
-
+                }
+            }
+        }
+    }
 }
