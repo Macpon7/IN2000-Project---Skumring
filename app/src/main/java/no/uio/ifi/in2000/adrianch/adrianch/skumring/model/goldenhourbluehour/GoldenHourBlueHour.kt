6a3de811eb9623c7aabb6bfbd7624ed71d9d39da
viljardh <viljardh@ifi.uio.no>
2024-04-26 14:44:55 +0200

    reformat response data

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/goldenhourbluehour/GoldenHourBlueHour.kt
+++
@@ -1,6 +0,0 @@
-package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.goldenhourbluehour
-
-data class GoldenHourBlueHour(
-    val results: Results,
-    val status: String
-)
---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/goldenhourbluehour/SunriseSunsetResponseData.kt
@@ -0,0 +1,21 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.goldenhourbluehour
+
+data class GoldenHourBlueHour(
+    val results: Results,
+    val status: String
+)
+
+data class Results(
+    val date: String,
+    val dawn: String,
+    val day_length: String,
+    val dusk: String,
+    val first_light: String,
+    val golden_hour: String,
+    val last_light: String,
+    val solar_noon: String,
+    val sunrise: String,
+    val sunset: String,
+    val timezone: String,
+    val utc_offset: Int
+)
