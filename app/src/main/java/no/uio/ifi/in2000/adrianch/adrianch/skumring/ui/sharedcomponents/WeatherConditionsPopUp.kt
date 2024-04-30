60544a65bfab4298405bdefca74b34f0ea8d5909
annhansg <annhansg@ulrik.uio.no>
2024-04-30 13:01:01 +0200

    fix formatting

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/WeatherConditionsPopUp.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/WeatherConditionsPopUp.kt
@@ -1,54 +1,33 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.sharedcomponents
 
 
-import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Divider
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.R
-
-
 
 
 @Composable
@@ -62,8 +41,7 @@ fun WeatherIconPopUp(
         onDismissRequest = onClose
     ) {
         Surface(
-            shape = MaterialTheme.shapes.medium,
-            modifier = Modifier
+            shape = MaterialTheme.shapes.medium, modifier = Modifier
                 .padding(16.dp)
                 .fillMaxWidth()
         ) {
@@ -71,10 +49,14 @@ fun WeatherIconPopUp(
                 modifier = Modifier.padding(16.dp)
             ) {
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.Top
+                    modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.Top
                 ) {
-                    Icon(Icons.Default.Info, contentDescription = "Info", tint = textColor, modifier = Modifier.size(30.dp))
+                    Icon(
+                        Icons.Default.Info,
+                        contentDescription = "Info",
+                        tint = textColor,
+                        modifier = Modifier.size(30.dp)
+                    )
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(
                         text = stringResource(id = R.string.weather_condition),
@@ -84,8 +66,8 @@ fun WeatherIconPopUp(
                 }
                 Spacer(modifier = Modifier.height(20.dp))
 
-                Row{
-                    Column{
+                Row {
+                    Column {
                         Text(
                             text = stringResource(R.string.conditions_excellent),
                             style = MaterialTheme.typography.bodyLarge,
@@ -106,8 +88,8 @@ fun WeatherIconPopUp(
                     }
                 }
                 Spacer(modifier = Modifier.height(16.dp))
-                Row{
-                    Column{
+                Row {
+                    Column {
                         Text(
                             text = stringResource(R.string.conditions_decent),
                             style = MaterialTheme.typography.bodyLarge,
@@ -128,8 +110,8 @@ fun WeatherIconPopUp(
                     }
                 }
                 Spacer(modifier = Modifier.height(16.dp))
-                Row{
-                    Column{
+                Row {
+                    Column {
                         Text(
                             text = stringResource(R.string.conditions_poor),
                             style = MaterialTheme.typography.bodyLarge,
@@ -151,26 +133,25 @@ fun WeatherIconPopUp(
                     }
                 }
 
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Row(
-                                horizontalArrangement = Arrangement.End,
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                TextButton(
-                                    onClick = onClose
-                                ) {
-                                    Text(
-                                        stringResource(id = R.string.weather_conditions_popUp_dismiss),
-                                        style = MaterialTheme.typography.bodyLarge,
-                                        color = textColor,
-                                        fontWeight = FontWeight.Bold)
-                                }
-                            }
-                        }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()
+                ) {
+                    TextButton(
+                        onClick = onClose
+                    ) {
+                        Text(
+                            stringResource(id = R.string.weather_conditions_popUp_dismiss),
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = textColor,
+                            fontWeight = FontWeight.Bold
+                        )
                     }
                 }
             }
-
+        }
+    }
+}
 
 
 @Preview
@@ -178,7 +159,7 @@ fun WeatherIconPopUp(
 fun CustomDialogPreview() {
     MaterialTheme {
         WeatherIconPopUp(
-            onClose = { /* Handle close action */ },
+            onClose = {},
         )
     }
 }
