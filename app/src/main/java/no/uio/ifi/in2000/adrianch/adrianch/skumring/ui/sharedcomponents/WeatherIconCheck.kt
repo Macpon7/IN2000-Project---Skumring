f572494d536128519ae72ebb7213c01295b59567
annhansg <annhansg@ulrik.uio.no>
2024-05-01 13:16:37 +0200

    add more icons and a check for excellent weather to WeatherIconCheck

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/WeatherIconCheck.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/WeatherIconCheck.kt
@@ -6,15 +6,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.R
-
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.forecast.WeatherConditionsRating
 
 /**
  * A check that visualizes weather conditions by displaying appropriate icons based on the provided weather condition string
  */
 @Composable
-fun WeatherIconCheck (weatherCondition: String) {
+fun WeatherIconCheck(weatherCondition: String, weather: WeatherConditionsRating) {
+    //If its thunder
     if (weatherCondition.contains("thunder")) {
         Icon(
             painterResource(id = R.drawable.thunder_icon),
@@ -22,6 +24,7 @@ fun WeatherIconCheck (weatherCondition: String) {
             tint = Color.Unspecified,
             modifier = Modifier.size(140.dp)
         )
+        //If it rains
     } else if (weatherCondition.contains("rain")) {
         Icon(
             painterResource(id = R.drawable.rain_icon),
@@ -29,6 +32,7 @@ fun WeatherIconCheck (weatherCondition: String) {
             tint = Color.Unspecified,
             modifier = Modifier.size(140.dp)
         )
+        //If its snowy
     } else if (weatherCondition.contains("snow")) {
         Icon(
             painterResource(id = R.drawable.snow_icon),
@@ -36,8 +40,8 @@ fun WeatherIconCheck (weatherCondition: String) {
             tint = Color.Unspecified,
             modifier = Modifier.size(140.dp)
         )
-
-        } else if (weatherCondition.contains("clear")) {
+        //If the weather is clear
+    } else if (weatherCondition.contains("clear")) {
         Icon(
             painterResource(id = R.drawable.clear_icon),
             contentDescription = "Weather icon clear",
@@ -45,6 +49,15 @@ fun WeatherIconCheck (weatherCondition: String) {
             modifier = Modifier.size(140.dp)
         )
 
+        //If weatherconditions is excellent and there are some clouds high in the higher layers of the sky
+    } else if (weather == WeatherConditionsRating.EXCELLENT && weatherCondition.contains("partly")) {
+        Icon(
+            painterResource(id = R.drawable.partly_cloudy_sun_icon),
+            contentDescription = "Weather icon clear",
+            tint = Color.Unspecified,
+            modifier = Modifier.size(140.dp)
+        )
+        //if there are some clouds are lower in the sky
     } else if (weatherCondition.contains("partly")) {
         Icon(
             painterResource(id = R.drawable.partly_cloudy_icon),
@@ -52,29 +65,41 @@ fun WeatherIconCheck (weatherCondition: String) {
             tint = Color.Unspecified,
             modifier = Modifier.size(140.dp)
         )
-    } else if ( weatherCondition.contains("fair")) {
+        //If weatherconditons is excellent and there are clouds in the higher layers of the sky
+    } else if (weather == WeatherConditionsRating.EXCELLENT && weatherCondition.contains("cloudy")) {
+        Icon(
+            painterResource(id = R.drawable.cloudy_sun_icon),
+            contentDescription = "Weather icon clear",
+            tint = Color.Unspecified,
+            modifier = Modifier.size(140.dp)
+        )
+        //If there are clouds lower in the sky
+    } else if (weatherCondition.contains("cloudy")) {
+        Icon(
+            painterResource(id = R.drawable.cloudy_icon),
+            contentDescription = "Weather icon cloudy",
+            tint = Color.Unspecified,
+            modifier = Modifier.size(140.dp)
+        )
+        //if the weather is fair
+    } else if (weatherCondition.contains("fair")) {
         Icon(
             painterResource(id = R.drawable.fair_icon),
             contentDescription = "Weather icon cloudy",
             tint = Color.Unspecified,
             modifier = Modifier.size(140.dp)
         )
-    }else if (weatherCondition.contains("cloudy")) {
-            Icon(
-                painterResource(id = R.drawable.cloudy_icon),
-                contentDescription = "Weather icon cloudy",
-                tint = Color.Unspecified,
-                modifier = Modifier.size(140.dp)
-            )
-        }else if ( weatherCondition.contains("fog")) {
-                Icon(
-                    painterResource(id = R.drawable.fog_icon),
-                    contentDescription = "Weather icon cloudy",
-                    tint = Color.Unspecified,
-                    modifier = Modifier.size(140.dp)
-                )
+        //if the weather is foggy
+    } else if (weatherCondition.contains("fog")) {
+        Icon(
+            painterResource(id = R.drawable.fog_icon),
+            contentDescription = "Weather icon cloudy",
+            tint = Color.Unspecified,
+            modifier = Modifier.size(140.dp)
+        )
+        //If none of the other checks work, show default image
     } else {
-        Icon ( //if none of the other checks work, show default image
+        Icon(
             painterResource(id = R.drawable.image_not_found),
             contentDescription = "Weather icon cloudy",
             tint = Color.Unspecified,
