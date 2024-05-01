beecb667709e2ad1c67c58dab73f9d3de4195c50
annhansg <annhansg@ulrik.uio.no>
2024-05-01 13:16:52 +0200

    add more icons and a check for excellent weather to WeatherIconCheck

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/WeatherIconCheck.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/sharedcomponents/WeatherIconCheck.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.R
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.forecast.WeatherConditionsRating
