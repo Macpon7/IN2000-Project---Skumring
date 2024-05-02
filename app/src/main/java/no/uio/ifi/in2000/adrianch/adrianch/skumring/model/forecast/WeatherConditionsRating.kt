c4ab22f49613b24a2216c19973bbf89abfeed899
marisnot <marisnot@ulrik.uio.no>
2024-05-02 15:07:56 +0200

    fix string to show correct text

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/forecast/WeatherConditionsRating.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/model/forecast/WeatherConditionsRating.kt
@@ -3,7 +3,7 @@ package no.uio.ifi.in2000.adrianch.adrianch.skumring.model.forecast
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.R
 
 /**
- * Rating between 1-3 stars depending on conditions
+ * Rating between "poor", "OK" and "excellent" depending on conditions
  */
 enum class WeatherConditionsRating(val stringResourceId: Int) {
     POOR(stringResourceId = R.string.conditions_poor),
