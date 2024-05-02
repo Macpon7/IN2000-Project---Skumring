731f586eaef073e41c3f50e6453a915eddcb2162
viljardh <viljardh@ifi.uio.no>
2024-05-03 00:45:09 +0200

    tested GHBHDataSource

--- app/src/test/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/goldenhourbluehour/GoldenHourBlueHourDataSourceTest.kt
+++ app/src/test/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/goldenhourbluehour/GoldenHourBlueHourDataSourceTest.kt
@@ -1,7 +1,6 @@
 package no.uio.ifi.in2000.adrianch.adrianch.skumring.goldenhourbluehour
 
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.goldenhourbluehour.GoldenHourBlueHourDataSource
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.goldenhourbluehour.SunriseSunset
 import org.junit.Test
@@ -23,14 +22,31 @@ class GoldenHourBlueHourDataSourceTest {
         SunriseSunset::class.java
     )
 
-
+    /*
+    Testing that a response gets correctly converted to our custom object
+    as well as the time getting converted correctly
+     */
     @Test
     fun checkFixTimeFormat() {
         val formatter = DateTimeFormatter.ofPattern("HH':'mm")
-
         val expected = "13:26"
         val responseDateTime = source.convertResponseToGoldenHourBlueHour(osloTestData, dummyDate).goldenHour
         val result = responseDateTime.format(formatter)
+
+        assert(expected == result)
+    }
+
+    /*
+    This checks for blue/golden hour at the north pole, but above certain latitudes
+    the sun doesn't set during winter, so this would be relevant for locations
+    above the polar circle.
+     */
+    @Test
+    fun checkNoGoldenOrBlueHour() {
+        val expected = "2000-01-01T00:00"
+        val responseDateTime = source.convertResponseToGoldenHourBlueHour(northPoleTestData,dummyDate).blueHour
+        val result = responseDateTime.toString()
+
         assert(expected == result)
     }
 }
