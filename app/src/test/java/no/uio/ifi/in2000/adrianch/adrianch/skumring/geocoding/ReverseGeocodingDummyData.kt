654c934b8b7cfa42efb877c0887a201a1558949c
viljardh <viljardh@ifi.uio.no>
2024-05-03 13:51:06 +0200

    add dummy data

---
+++ app/src/test/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/geocoding/ReverseGeocodingDummyData.kt
@@ -0,0 +1,231 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.geocoding
+
+class ReverseGeocodingDummyData {
+    val ojdReverseGeo: String = """
+        {
+        "type": "FeatureCollection",
+        "features": [
+        {
+        "type": "Feature",
+        "id": "dXJuOm1ieGFkcjpkZjFiNGRiZi1jOGYyLTRmNDctYjBlYS03MzNmNmQzM2VkMDE",
+        "geometry": {
+        "type": "Point",
+        "coordinates": [
+        10.717823,
+        59.94342
+        ]
+        },
+        "properties": {
+        "mapbox_id": "dXJuOm1ieGFkcjpkZjFiNGRiZi1jOGYyLTRmNDctYjBlYS03MzNmNmQzM2VkMDE",
+        "feature_type": "address",
+        "full_address": "Gaustadalléen 23b, 0373 Oslo, Norway",
+        "name": "Gaustadalléen 23b",
+        "name_preferred": "Gaustadalléen 23b",
+        "coordinates": {
+        "longitude": 10.717823,
+        "latitude": 59.94342,
+        "accuracy": "rooftop",
+        "routable_points": [
+        {
+        "name": "default",
+        "latitude": 59.943889316306304,
+        "longitude": 10.716866052604727
+        }
+        ]
+        },
+        "place_formatted": "0373 Oslo, Norway",
+        "context": {
+        "address": {
+        "mapbox_id": "dXJuOm1ieGFkcjpkZjFiNGRiZi1jOGYyLTRmNDctYjBlYS03MzNmNmQzM2VkMDE",
+        "address_number": "23b",
+        "street_name": "Gaustadalléen",
+        "name": "Gaustadalléen 23b"
+        },
+        "street": {
+        "mapbox_id": "dXJuOm1ieGFkcjpkZjFiNGRiZi1jOGYyLTRmNDctYjBlYS03MzNmNmQzM2VkMDE",
+        "name": "Gaustadalléen"
+        },
+        "postcode": {
+        "mapbox_id": "dXJuOm1ieHBsYzpEZzZw",
+        "name": "0373"
+        },
+        "place": {
+        "mapbox_id": "dXJuOm1ieHBsYzpBU1Nw",
+        "name": "Oslo",
+        "wikidata_id": "Q585",
+        "short_code": "NO-03"
+        },
+        "region": {
+        "mapbox_id": "dXJuOm1ieHBsYzpBU1Nw",
+        "name": "Oslo",
+        "wikidata_id": "Q585",
+        "region_code": "03",
+        "region_code_full": "NO-03"
+        },
+        "country": {
+        "mapbox_id": "dXJuOm1ieHBsYzpJcWs",
+        "name": "Norway",
+        "wikidata_id": "Q20",
+        "country_code": "NO",
+        "country_code_alpha_3": "NOR"
+        }
+        }
+        }
+        },
+        {
+        "type": "Feature",
+        "id": "dXJuOm1ieHBsYzpEZzZw",
+        "geometry": {
+        "type": "Point",
+        "coordinates": [
+        10.711873,
+        59.942496
+        ]
+        },
+        "properties": {
+        "mapbox_id": "dXJuOm1ieHBsYzpEZzZw",
+        "feature_type": "postcode",
+        "full_address": "0373, Oslo, Norway",
+        "name": "0373",
+        "name_preferred": "0373",
+        "coordinates": {
+        "longitude": 10.711873,
+        "latitude": 59.942496
+        },
+        "place_formatted": "Oslo, Norway",
+        "bbox": [
+        10.702206,
+        59.936052,
+        10.720296,
+        59.948789
+        ],
+        "context": {
+        "region": {
+        "mapbox_id": "dXJuOm1ieHBsYzpBU1Nw",
+        "name": "Oslo",
+        "wikidata_id": "Q585",
+        "region_code": "03",
+        "region_code_full": "NO-03"
+        },
+        "place": {
+        "mapbox_id": "dXJuOm1ieHBsYzpBU1Nw",
+        "name": "Oslo",
+        "wikidata_id": "Q585",
+        "short_code": "NO-03"
+        },
+        "country": {
+        "mapbox_id": "dXJuOm1ieHBsYzpJcWs",
+        "name": "Norway",
+        "wikidata_id": "Q20",
+        "country_code": "NO",
+        "country_code_alpha_3": "NOR"
+        },
+        "postcode": {
+        "mapbox_id": "dXJuOm1ieHBsYzpEZzZw",
+        "name": "0373"
+        }
+        }
+        }
+        },
+        {
+        "type": "Feature",
+        "id": "dXJuOm1ieHBsYzpBU1Nw",
+        "geometry": {
+        "type": "Point",
+        "coordinates": [
+        10.7389701,
+        59.9133301
+        ]
+        },
+        "properties": {
+        "mapbox_id": "dXJuOm1ieHBsYzpBU1Nw",
+        "feature_type": "place",
+        "full_address": "Oslo, Norway",
+        "name": "Oslo",
+        "name_preferred": "Oslo",
+        "coordinates": {
+        "longitude": 10.7389701,
+        "latitude": 59.9133301
+        },
+        "additional_feature_types": [
+        "region",
+        "place"
+        ],
+        "place_formatted": "Norway",
+        "bbox": [
+        10.489164,
+        59.809333,
+        10.951387,
+        60.135106
+        ],
+        "context": {
+        "region": {
+        "mapbox_id": "dXJuOm1ieHBsYzpBU1Nw",
+        "name": "Oslo",
+        "wikidata_id": "Q585"
+        },
+        "country": {
+        "mapbox_id": "dXJuOm1ieHBsYzpJcWs",
+        "name": "Norway",
+        "wikidata_id": "Q20",
+        "country_code": "NO",
+        "country_code_alpha_3": "NOR"
+        },
+        "place": {
+        "mapbox_id": "dXJuOm1ieHBsYzpBU1Nw",
+        "name": "Oslo",
+        "wikidata_id": "Q585"
+        }
+        }
+        }
+        },
+        {
+        "type": "Feature",
+        "id": "dXJuOm1ieHBsYzpJcWs",
+        "geometry": {
+        "type": "Point",
+        "coordinates": [
+        8.80482073895072,
+        61.2483331493152
+        ]
+        },
+        "properties": {
+        "mapbox_id": "dXJuOm1ieHBsYzpJcWs",
+        "feature_type": "country",
+        "full_address": "Norway",
+        "name": "Norway",
+        "name_preferred": "Norway",
+        "coordinates": {
+        "longitude": 8.80482073895072,
+        "latitude": 61.2483331493152
+        },
+        "bbox": [
+        4.4014523,
+        57.9083581,
+        31.2678845,
+        71.215707
+        ],
+        "context": {
+        "country": {
+        "mapbox_id": "dXJuOm1ieHBsYzpJcWs",
+        "name": "Norway",
+        "country_code": "NO",
+        "country_code_alpha_3": "NOR",
+        "wikidata_id": "Q20"
+        }
+        }
+        }
+        }
+        ],
+        "attribution": "NOTICE: © 2024 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service (https://www.mapbox.com/about/maps/). This response and the information it contains may not be retained."
+        }
+    """.trimIndent()
+
+    val nullIslandReverseGeo: String = """
+        {
+        "type": "FeatureCollection",
+        "features": [],
+        "attribution": "NOTICE: © 2024 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service (https://www.mapbox.com/about/maps/). This response and the information it contains may not be retained."
+        }
+    """.trimIndent()
+}
