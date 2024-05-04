5da3bd122a09842e3dda4bde993a4f1a16d67602
synnovnh <synnovnh@ulrik.uio.no>
2024-05-04 15:20:38 +0200

    implements stringResourceId in enum classes

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/settings/SettingsOptions.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/settings/SettingsOptions.kt
@@ -5,24 +5,25 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.ui.graphics.vector.ImageVector
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.R
 
 // Enum classes with settings
 
 // Lag det som alpaca
 
-enum class Theme() {
-    FOLLOW_SYSTEM,
-    DARK_MODE,
-    LIGHT_MODE
+enum class Theme(val stringResourceId: Int) {
+    FOLLOW_SYSTEM(R.string.follow_system),
+    DARK_MODE(R.string.dark_mode),
+    LIGHT_MODE(R.string.light_mode)
 }
 
-enum class Language {
-    FOLLOW_SYSTEM,
-    NORWEGIAN,
-    ENGLISH
+enum class Language(val stringResourceId: Int) {
+    FOLLOW_SYSTEM(R.string.follow_system),
+    NORWEGIAN(R.string.norwegian),
+    ENGLISH(R.string.english)
 }
 
-enum class Location {
-    COSTUM_LOCATION,
-    PHONES_LOCATION
+enum class Location(val stringResourceId: Int) {
+    COSTUM_LOCATION(R.string.costum_location),
+    PHONES_LOCATION(R.string.phones_location)
 }
