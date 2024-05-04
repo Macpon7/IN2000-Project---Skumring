37f15b16dd5888be8fdf4df426d4df852c42efaa
viljardh <viljardh@ifi.uio.no>
2024-05-04 23:13:07 +0200

    test walking duration and distance

--- app/src/test/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/directions/DirectionsDataSourceTest.kt
+++ app/src/test/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/directions/DirectionsDataSourceTest.kt
@@ -3,11 +3,15 @@ package no.uio.ifi.in2000.adrianch.adrianch.skumring.directions
 import com.google.gson.Gson
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.directions.DirectionsDataSource
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.directions.Directions
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.directions.MeansOfTransportation
 import org.junit.Test
 
 class DirectionsDataSourceTest {
     private val gson = Gson()
     private val source = DirectionsDataSource()
+    private val walking = MeansOfTransportation.WALKING
+    private val biking = MeansOfTransportation.BIKING
+    private val driving = MeansOfTransportation.DRIVING
 
     private val walkingUioToOsloMetTestData: Directions = gson.fromJson(
         walkingUioToOsloMet,
@@ -25,4 +29,28 @@ class DirectionsDataSourceTest {
         drivingNullIslandToOsloMet,
         Directions::class.java
     )
+
+    /*
+    Testing that the responses get properly converted from json as we can see with
+    our own eyes to the object containing the data we want.
+     */
+    @Test
+    fun checkWalkingDurationDistance() {
+        val expectedHours = "0"
+        val expectedMinutes = "37"
+        val expectedDistance = "3.3"
+        val responseMinutes = source.convertResponseToTravelDurationDistance(
+            walkingUioToOsloMetTestData,
+            walking).durationMinutes
+        val responseHours = source.convertResponseToTravelDurationDistance(
+            walkingUioToOsloMetTestData,
+            walking).durationHours
+        val responseDistance = source.convertResponseToTravelDurationDistance(
+            walkingUioToOsloMetTestData,
+            walking).distance
+        assert((expectedDistance == responseDistance &&
+                expectedMinutes == responseMinutes &&
+                expectedHours == responseHours))
+
+    }
 }
