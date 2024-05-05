4c9a2fb9395e2b557f3a11f2c47187875a5fa73d
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-05-05 21:41:53 +0200

    Move NewPlaceDialog to its own file and give it its own viewmodel

---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/dialogs/NewPlaceDialog.kt
@@ -0,0 +1,336 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.dialogs
+
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Create
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.R
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.mypage.NewPlaceUiState
+import java.time.format.DateTimeFormatter
+
+private const val TAG = "NewPlaceDialog"
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewPlaceDialog(
+    hideDialog: () -> Unit,
+    newPlaceViewModel: NewPlaceViewModel = viewModel(factory = NewPlaceViewModel.Factory)
+) {
+    LaunchedEffect(Unit) {
+        newPlaceViewModel.resetNewPlaceUiState()
+    }
+
+    val newPlaceUiState: NewPlaceUiState by newPlaceViewModel.newPlaceUiState.collectAsState()
+
+    // TODO keep the change when the phone change from standing to lying
+
+    val outlinedTextFieldColors = ExposedDropdownMenuDefaults.textFieldColors(
+        focusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+        focusedTextColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        focusedIndicatorColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        focusedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        focusedLeadingIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
+
+        unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+        unfocusedTextColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        unfocusedIndicatorColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        unfocusedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        unfocusedLeadingIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
+
+        errorTextColor = MaterialTheme.colorScheme.onPrimary,
+        errorContainerColor = MaterialTheme.colorScheme.primaryContainer,
+        errorCursorColor = MaterialTheme.colorScheme.error,
+        errorIndicatorColor = MaterialTheme.colorScheme.error,
+        errorLeadingIconColor = MaterialTheme.colorScheme.error,
+    )
+
+    // Show when the user pick a date:
+    if (newPlaceUiState.showDatePicker) {
+        DatePickerDialog(onDismissRequest = { newPlaceViewModel.dismissDatePicker() },
+            confirmButton = {
+                TextButton(onClick = {
+                    newPlaceViewModel.saveSelectedDate()
+                }) {
+                    Text(
+                        text = stringResource(R.string.add_date),
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    newPlaceViewModel.dismissDatePicker()
+                }) {
+                    Text(
+                        text = stringResource(R.string.cancel),
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+
+        ) {
+            DatePicker(state = newPlaceUiState.datePickerState)
+        }
+    }
+
+    //This is the dialog for adding a new place
+    Dialog(onDismissRequest = {
+        newPlaceViewModel.resetNewPlaceUiState()
+        hideDialog()
+    }) {
+
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            )
+        ) {
+            Column(
+                modifier = Modifier.padding(all = 8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.mypage_fill_in_fields),
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        bottom = 4.dp,
+                        top = 6.dp
+                    ),
+                    fontSize = 16.sp
+                )
+
+                // String with name
+                OutlinedTextField(
+                    modifier = Modifier.padding(all = 2.dp),
+                    value = newPlaceUiState.locationName,
+                    // Take variable from newPlaceUiState
+                    onValueChange = { newPlaceViewModel.updateNewLocationName(it) },
+                    label = {
+                        Text(
+                            text = "${stringResource(R.string.location_name)} *",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    singleLine = true,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Info,
+                            contentDescription = stringResource(id = R.string.location_name),
+                        )
+                    },
+                    supportingText = {
+                        if (newPlaceUiState.locationNameIsMissing) {
+                            Text(
+                                text = stringResource(R.string.error_location_name),
+                                color = MaterialTheme.colorScheme.error,
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                    },
+                    colors = outlinedTextFieldColors,
+                    isError = (newPlaceUiState.locationNameIsMissing)
+                )
+
+                // Brukeren skal skrive inn addresse:
+                OutlinedTextField(
+                    modifier = Modifier.padding(all = 2.dp),
+                    value = newPlaceUiState.address,
+                    onValueChange = { newPlaceViewModel.updateNewLocationAddress(it) },
+                    label = {
+                        Text(
+                            text = "${stringResource(R.string.address)} *",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    singleLine = true,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.LocationOn,
+                            contentDescription = stringResource(id = R.string.address),
+                        )
+                    },
+                    supportingText = {
+                        if (newPlaceUiState.addressIsMissing) {
+                            Text(
+                                text = stringResource(R.string.error_address),
+                                color = MaterialTheme.colorScheme.error,
+                                style = MaterialTheme.typography.bodyMedium,
+                            )
+                        } else if (newPlaceUiState.addressNoResults) {
+                            Text(
+                                text = stringResource(id = R.string.new_place_address_no_results),
+                                color = MaterialTheme.colorScheme.error
+                            )
+                        } else if (newPlaceUiState.addressTooManyResults) {
+                            Text(
+                                text = stringResource(id = R.string.new_place_address_many_results),
+                                color = MaterialTheme.colorScheme.error
+                            )
+                        }
+                    },
+                    colors = outlinedTextFieldColors,
+                    isError = newPlaceUiState.addressIsMissing
+                )
+                // Time skal lages made datepicker dialog
+                OutlinedTextField(
+                    value = newPlaceUiState.pickedDate.format(
+                        DateTimeFormatter.ISO_LOCAL_DATE
+                    ),
+                    onValueChange = { },
+                    modifier = Modifier
+                        .clickable(
+                            enabled = true,
+                            onClick = {
+                                newPlaceViewModel.showDatePicker()
+                            }
+                        )
+                        .padding(all = 2.dp),
+                    enabled = false,
+                    readOnly = true,
+                    isError = newPlaceUiState.dateTextFieldError,
+                    label = {
+                        Text(
+                            text = stringResource(R.string.choose_date),
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.DateRange,
+                            contentDescription = stringResource(id = R.string.time),
+                            tint = MaterialTheme.colorScheme.onPrimaryContainer
+                        )
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(
+                        // Make it look not disabled
+                        disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                        disabledTextColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                        disabledIndicatorColor = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                )
+
+                // String with description
+                OutlinedTextField(
+                    modifier = Modifier.padding(all = 2.dp),
+                    value = newPlaceUiState.description,
+                    onValueChange = { newPlaceViewModel.updateNewLocationDescription(it) },
+                    label = {
+                        Text(
+                            text = "${stringResource(R.string.description)} *",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    singleLine = true,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Create,
+                            contentDescription = stringResource(id = R.string.description)
+                        )
+                    },
+                    supportingText = {
+                        if (newPlaceUiState.descriptionIsMissing) {
+                            Text(
+                                text = stringResource(R.string.error_description),
+                                color = MaterialTheme.colorScheme.error,
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                    },
+                    colors = outlinedTextFieldColors,
+                    isError = newPlaceUiState.descriptionIsMissing,
+                )
+
+                // Button to add photo
+                val launcher =
+                    rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+                        newPlaceUiState.imageUri = uri
+                        Log.d(TAG, "imageUri is $newPlaceUiState.imageUri")
+                    }
+
+                Button(
+                    onClick = { launcher.launch("image/*") },
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary)
+                ) {
+                    Text(
+                        text = stringResource(R.string.add_photo),
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Icon(
+                        imageVector = Icons.Outlined.Add,
+                        contentDescription = stringResource(id = R.string.add_photo),
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+
+                // Button that is pressed when the location is added:
+                Button(
+                    onClick = { newPlaceViewModel.addLocation() },
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary)
+                ) {
+                    Text(
+                        text = stringResource(R.string.add_location),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Icon(
+                        imageVector = Icons.Outlined.Check,
+                        contentDescription = stringResource(id = R.string.add_location),
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+
+                if (newPlaceUiState.missingInfo) {
+                    Text(
+                        text = stringResource(R.string.missing_fields),
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/dialogs/NewPlaceViewModel.kt
@@ -0,0 +1,311 @@
+package no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.dialogs
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.ApplicationSkumring
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.geocoding.GeocodingRepository
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.geocoding.GeocodingRepositoryImpl
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.data.place.PlaceRepository
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.model.place.PlaceInfo
+import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.mypage.NewPlaceUiState
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+private const val TAG = "NewPlaceViewModel"
+
+data class NewPlaceUiState @OptIn(ExperimentalMaterial3Api::class) constructor(
+    var locationName: String = "",
+    var locationNameIsMissing: Boolean = false,
+
+    var address: String = "",
+    var addressIsMissing: Boolean = false,
+    var addressNoResults: Boolean = false,
+    var addressTooManyResults: Boolean = false,
+
+    var datePickerState: DatePickerState = DatePickerState(
+        initialSelectedDateMillis = null,
+        initialDisplayedMonthMillis = null,
+        yearRange = DatePickerDefaults.YearRange,
+        initialDisplayMode = DisplayMode.Picker
+    ),
+
+    // This will not make an error since if it is not picked it will be the current date:
+    var pickedDate: LocalDate = LocalDate.now(),
+
+    var description: String = "",
+    var descriptionIsMissing: Boolean = false,
+
+    // Variables for picture:
+    var imageUri: Uri? = null,
+
+    // Show the date picker when the user want to pick a date
+    var showDatePicker: Boolean = false,
+
+    // Show an error if the use pressed ok without picking a date
+    var datePickerError: Boolean = false,
+
+    // Show an error if user closes date picker without picking a date
+    var dateTextFieldError: Boolean = false,
+
+    // Check if something in the textfield is missing
+    var missingInfo: Boolean = false
+)
+class NewPlaceViewModel(
+    private val placeRepository: PlaceRepository,
+    private val geocodingRepository: GeocodingRepository = GeocodingRepositoryImpl(),
+    private val context: Context
+    ) : ViewModel() {
+    private val _newPlaceUiState = MutableStateFlow(NewPlaceUiState())
+    val newPlaceUiState: StateFlow<NewPlaceUiState> = _newPlaceUiState
+
+    init {
+
+    }
+
+    /**
+     * Functions for updating NewPlaceUiState:
+     *
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun resetNewPlaceUiState() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _newPlaceUiState.update { currentNewPlaceUiState ->
+                currentNewPlaceUiState.copy(
+                    locationName = "",
+                    locationNameIsMissing = false,
+                    address = "",
+                    addressIsMissing = false,
+                    addressTooManyResults = false,
+                    addressNoResults = false,
+                    description = "",
+                    descriptionIsMissing = false,
+                    imageUri = null,
+                    // TODO add bitmap ?
+                    pickedDate = LocalDate.now(),
+                    missingInfo = false
+                )
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun addLocation() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _newPlaceUiState.update { currentNewPlaceUiState ->
+                var isNameMissing = false
+                var isDescriptionMissing = false
+                var isAddressMissing = false
+                var addressNoResults = false
+                var addressTooManyResults = false
+
+                var isReady = true
+
+                // Check that input fields are good
+                if (currentNewPlaceUiState.locationName == "") {
+                    isNameMissing = true
+                    isReady = false
+                }
+                if (currentNewPlaceUiState.description == "") {
+                    isDescriptionMissing = true
+                    isReady = false
+                }
+                if (currentNewPlaceUiState.address == "") {
+                    isAddressMissing = true
+                    isReady = false
+                }
+
+                val addresses =
+                    geocodingRepository.getCoordinatesFromAddress(address = currentNewPlaceUiState.address)
+
+                if (addresses.size == 0) {
+                    //TODO set correct error and set error message under address field
+                    addressNoResults = true
+                    isReady = false
+                } else if (addresses.size > 1) {
+                    //TODO set correct error and set message under address field
+                    addressTooManyResults = true
+                    isReady = false
+                }
+
+                //TODO add option to use User's current location instead of writing in an address
+
+                if (!isReady) {
+                    currentNewPlaceUiState.copy(
+                        locationNameIsMissing = isNameMissing,
+                        addressIsMissing = isAddressMissing,
+                        descriptionIsMissing = isDescriptionMissing,
+                        addressNoResults = addressNoResults,
+                        addressTooManyResults = addressTooManyResults
+                    )
+                } else {
+                    Log.d(TAG, "Trying to add new custom place to DB")
+                    val newId = placeRepository.addCustomPlace(
+                        PlaceInfo(
+                            id = 0,
+                            name = currentNewPlaceUiState.locationName,
+                            description = currentNewPlaceUiState.description,
+                            lat = addresses.first().lat,
+                            long = addresses.first().long,
+                            isFavourite = false,
+                            isCustomPlace = true,
+                            hasNotification = false,
+                            images = emptyList(),
+                            sunEvents = emptyList()
+                        )
+                    )
+
+                    Log.d(TAG, "In MyPageViewModel Uri is ${newPlaceUiState.value.imageUri}")
+                    newPlaceUiState.value.imageUri?.let {
+                        addImage(
+                            contentUri = it,
+                            placeId = newId,
+                            timestamp = newPlaceUiState.value.pickedDate
+                        ) //TODO make this the placeId that is generated for the new place
+                        Log.d(TAG, "saving image")
+                    }
+                    currentNewPlaceUiState.copy()
+                }
+            }
+        }
+    }
+
+    /**
+     * The function will add an image to the database by adding the path to the internal location where the image is stored
+     */
+    private suspend fun addImage(contentUri: Uri, placeId: Int, timestamp: LocalDate) {
+        Log.d(TAG, "contentUri: $contentUri, placeId is $placeId")
+        val succeeded: Boolean = placeRepository.saveImageToInternalStorage(
+            context = context,
+            contentUri = contentUri,
+            placeId = placeId,
+            timestamp = timestamp
+        )
+        Log.d(TAG, "is image added to internal storage: $succeeded")
+    }
+
+    /**
+     * Update if datepicker should be shown
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun showDatePicker() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _newPlaceUiState.update { currentNewPlaceUiState ->
+                currentNewPlaceUiState.copy(
+                    showDatePicker = !currentNewPlaceUiState.showDatePicker
+                )
+            }
+        }
+    }
+
+    /**
+     * Update if the datepicker is dismissed
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun dismissDatePicker() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _newPlaceUiState.update { currentNewPlaceUiState ->
+                currentNewPlaceUiState.copy(
+                    dateTextFieldError = true,
+                    showDatePicker = !currentNewPlaceUiState.showDatePicker
+                )
+            }
+        }
+    }
+
+    /**
+     * Function used to save the date picked in MyPageScreen of the user
+     * It update the date picked
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun saveSelectedDate() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _newPlaceUiState.update { currentNewPlaceUiState ->
+                val dateFromPicker = currentNewPlaceUiState.datePickerState.selectedDateMillis
+
+                if (dateFromPicker == null) {
+                    currentNewPlaceUiState.copy(
+                        datePickerError = true
+                    )
+                } else {
+                    val date = LocalDateTime.ofEpochSecond(
+                        dateFromPicker / 1000, 0, ZoneOffset.UTC
+                    ).toLocalDate()!!
+                    currentNewPlaceUiState.copy(
+                        datePickerError = false,
+                        showDatePicker = !currentNewPlaceUiState.showDatePicker,
+                        pickedDate = date
+                    )
+                }
+            }
+        }
+    }
+
+    /**
+     * update the location name string in NewPlaceUiState
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun updateNewLocationName(locationName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _newPlaceUiState.update { currentNewPlaceUiState ->
+                currentNewPlaceUiState.copy(locationName = locationName)
+            }
+        }
+    }
+
+    /**
+     * update the location name string in NewPlaceUiState
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun updateNewLocationAddress(address: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _newPlaceUiState.update { currentNewPlaceUiState ->
+                currentNewPlaceUiState.copy(address = address)
+            }
+        }
+    }
+
+    /**
+     * update the location description string in NewPlaceUiState
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun updateNewLocationDescription(descriptions: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _newPlaceUiState.update { currentNewPlaceUiState ->
+                currentNewPlaceUiState.copy(description = descriptions)
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    companion object {
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras,
+            ): T {
+                val application =
+                    checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]) as ApplicationSkumring
+
+                return NewPlaceViewModel(
+                    placeRepository = application.dbRepository,
+                    context = application.context
+                ) as T
+            }
+        }
+    }
+}
