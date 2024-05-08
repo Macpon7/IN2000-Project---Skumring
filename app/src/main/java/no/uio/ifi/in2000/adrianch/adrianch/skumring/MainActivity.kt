c26987dfe412e3840efa92d79954f8a0be19fae6
marisnot <marisnot@ulrik.uio.no>
2024-05-08 14:02:45 +0200

    add so splashscreen is shown first in MainActivity.kt

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/MainActivity.kt
@@ -3,15 +3,26 @@ package no.uio.ifi.in2000.adrianch.adrianch.skumring
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import no.uio.ifi.in2000.adrianch.adrianch.skumring.ui.theme.SkumringTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val mainViewModel: MainViewModel by viewModels()
+
+        installSplashScreen().apply{
+            setKeepOnScreenCondition {
+                mainViewModel.mainScreenUiState.value.splashScreenReady
+            }
+        }
+
         setContent {
             SkumringTheme {
                 // A surface container using the 'background' color from the theme
