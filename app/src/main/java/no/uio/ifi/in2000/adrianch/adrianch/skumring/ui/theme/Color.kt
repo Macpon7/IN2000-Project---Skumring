5125ac65d7d23f6db3ec62456ce756cd0618e238
annhansg <annhansg@ulrik.uio.no>
2024-05-11 23:26:46 +0200

    fix colorTheme

--- app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/theme/Color.kt
+++ app/src/main/java/no/uio/ifi/in2000/adrianch/adrianch/skumring/ui/theme/Color.kt
@@ -7,62 +7,37 @@ import androidx.compose.ui.graphics.Color
  * if the color is used in another screen as well it is commented out
  */
 
+
 //HOMESCREEN COLORS
 val inverseSurfaceDark = Color(0xFF151F38) //Sunset gradient colors
 val inverseOnSurfaceDark = Color(0xFF1F4A6C) //Sunset gradient colors
 val inversePrimaryDark = Color(0xFF45455E) //Sunset gradient colors
-val primaryDark = Color(0xFFAEC6FF)  //sunset today text
-val onPrimaryContainerDark = Color(0xFFD8E2FF) //SunsetInfoCard text color
 
-//MAPLIST COLORS
-val secondaryDark = Color(0xFFBFADFF) //favourite heart color
-val tertiaryDark = Color(0xFFB5C4FF) //Togglebutton text color
-val onTertiaryDark = Color(0xFF1C2D61)//Togglebutton background color
-val primaryContainerDark = Color(0xFF2C4678) //card background color
-//val onPrimaryContainerDark = Color(0xFFD8E2FF) //card text color
-
-//MAPLISTSCREEN POPUP
-//val surfaceVariantDark = Color(0xFF1E1E1E) // background color
-//val onSurfaceVariantDark = Color(0xFFC5C6D0) //text color
-// val primaryDark = Color(0xFFAEC6FF) //headline text
-//val secondaryDark = Color(0xFFBFADFF) //favourite heart color
-//val secondaryContainerDark = Color(0xFF4B3E76)//button background color
-
-//PLACEINFOSCREEN COLORS
-val surfaceVariantDark = Color(0xFF1E1E1E) // background color
-val onSurfaceVariantDark = Color(0xFFC5C6D0) //text color
-val secondaryContainerDark = Color(0xFF4B3E76)//button background color
-val onSecondaryContainerDark = Color(0xFFE7DEFF) //button text color
-// val primaryDark = Color(0xFFAEC6FF) //headline text
-//val secondaryDark = Color(0xFFBFADFF) //favourite heart color
-
-
-//MYPAGE
-val errorDark = Color(0xFFFFB4AB) //error color
-//val surfaceVariantDark = Color(0xFF1E1E1E) // background color
-//val secondaryContainerDark = Color(0xFF4B3E76)//MyPage button background color
-//val onSecondaryContainerDark = Color(0xFFE7DEFF) //MyPage button text color
-//val onPrimaryContainerDark = Color(0xFFD8E2FF) // text color
-
-//SETTINGS
-val backgroundDark = Color(0xFF121318) //background color
-val onSurfaceDark = Color(0xFFE2E2E9) //text color
-val onSecondaryDark = Color(0xFF34275E) //container color
-
-//WEATHERCONDITIONS POPUP
-//val primaryDark = Color(0xFFAEC6FF) //Headline text and "close" text color
-//val onSurfaceVariantDark = Color(0xFFC5C6D0) //text color
-
-//ADDITIONAL COLORS NOT USED
+val primaryDark = Color(0xFFAEC6FF)
 val onPrimaryDark = Color(0xFF122F60)
+val primaryContainerDark = Color(0xFF2C4678)
+val onPrimaryContainerDark = Color(0xFFD8E2FF)
+val secondaryDark = Color(0xFFBFADFF)
+val onSecondaryDark = Color(0xFF34275E)
+val secondaryContainerDark = Color(0xFF4B3E76)
+val onSecondaryContainerDark = Color(0xFFE7DEFF)
+val tertiaryDark = Color(0xFFB5C4FF)
+val onTertiaryDark = Color(0xFF1C2D61)
 val tertiaryContainerDark = Color(0xFF344479)
 val onTertiaryContainerDark = Color(0xFFDCE1FF)
+val surfaceVariantDark = Color(0xFF1E1E1E)
+val onSurfaceVariantDark = Color(0xFFC5C6D0)
+val backgroundDark = Color(0xFF121318)
+val onSurfaceDark = Color(0xFFE2E2E9)
+
+//ADDITIONAL COLORS NOT USED
+val errorDark = Color(0xFFFFB4AB)
 val onErrorDark = Color(0xFF690005)
 val errorContainerDark = Color(0xFF93000A)
 val onErrorContainerDark = Color(0xFFFFDAD6)
 val onBackgroundDark = Color(0xFFE2E2E9)
 val outlineDark = Color(0xFF8E9099)
-val outlineVariantDark = Color(0xFF858FBC) //val outlineVariantDark = Color(0xFF44474F)
+val outlineVariantDark = Color(0xFF858FBC) //(0xFF44474F)
 val surfaceDark = Color(0xFF121318)
 val scrimDark = Color(0xFF1E1F25)
 
@@ -79,55 +54,25 @@ val scrimDark = Color(0xFF1E1F25)
 val inverseSurfaceLight = Color(0xFF0062A1)//Sunset gradient colors
 val inverseOnSurfaceLight = Color(0xFFC3E8FF)//Sunset gradient colors
 val inversePrimaryLight = Color(0xFFD8E2FF)//Sunset gradient colors
-val primaryLight = Color(0xFF001A42) //Sunset today text homescreen, original: 0xFF455E91,
-val onPrimaryContainerLight = Color(0xFF001A42) //SunsetInfoCard text color
-
-//MAPLIST COLORS //Fornøyd!
-val secondaryLight = Color(0xFF63568F) //favourite heart color
-val tertiaryLight = Color(0xFFFFFFFF) //Togglebutton text color
-val onTertiaryLight = Color(0xFF4C5C92) //Togglebutton background color
-val primaryContainerLight = Color(0xFFCEE5FF) //card background color //0xFFD8E2FF
-//val onPrimaryContainerLight = Color(0xFF001A42) //card text color
-
-//MAPLIST POPUP //Fornøyd!
-//val surfaceVariantLight = Color(0xFFE1E2EC) // background color
-//val onSurfaceVariantLight = Color(0xFF44474F) //text color
-//val primaryLight = Color(0xFF001A42) //Sunset today text homescreen, original: 0xFF455E91
-//val secondaryLight = Color(0xFF63568F) //favourite heart color
-//val onSecondaryContainerLight = Color(0xFF1F1048) //Button text color
-
-//PLACEINFOSCREEN COLORS //Fornøyd!
-val surfaceVariantLight = Color(0xFFCEE5FF) // background color //Original: 0xFFE1E2EC  //0xFFC3E8FF  //0xFFC3E8FF placeinfo nå
-val onSurfaceVariantLight = Color(0xFF35383F) //text color
-val secondaryContainerLight = Color(0xFF001A42) //Button background color //0xFFE7DEFF  //0xFF416AB9
-val onSecondaryContainerLight = Color(0xFFFFFFFF) //Button text color //0xFF1F1048
-val outlineVariantLight = Color(0xFF001A42)//headline text
-//val secondaryLight = Color(0xFF63568F) //favourite heart color
-
-//MYPAGE //fornøyd!
-val errorLight = Color(0xFFBA1A1A) //error color
-//val surfaceVariantLight = Color(0xFFE1E2EC) // background color
-// val secondaryContainerLight = Color(0xFFE7DEFF) //Button background color
-// val onSecondaryContainerLight = Color(0xFF1F1048) //Button text color
-//val onPrimaryContainerLight = Color(0xFF001A42) // text color
-
-//NEWPLACEDIALOG //fornøyd!
-//val primaryLight = Color(0xFF001A42) //Sunset today text homescreen, original: 0xFF455E91
 
 
-//SETTINGS --> må ordnes
-val onSurfaceLight = Color(0xFF1A1B20) //text color
-val onSecondaryLight = Color(0xFFD8E2FF) //container color //0xFFFFFFFF //0xFFCEE5FF
-//val backgroundLight = Color(0xFFFAF9FF) //background color
-
-//WEATHERCONDITIONS POPUP --> må ordnes
-// val primaryLight = Color(0xFF001A42) //Headline text and "close" text color
-// val onSurfaceVariantLight = Color(0xFF44474F) //text color
-
-//ADDITIONAL COLORS NOT USED
+val primaryLight = Color(0xFF001A42) // original: 0xFF455E91
 val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFFCEE5FF) //original: 0xFFD8E2FF
+val onPrimaryContainerLight = Color(0xFF001A42)
+val secondaryLight = Color(0xFF63568F)
+val onSecondaryLight = Color(0xFFD8E2FF) //original: 0xFFFFFFFF
+val secondaryContainerLight = Color(0xFF001A42) //original: 0xFF416AB9
+val onSecondaryContainerLight = Color(0xFFFFFFFF) //original: 0xFF1F1048
+val tertiaryLight = Color(0xFFFFFFFF)
+val onTertiaryLight = Color(0xFF4C5C92)
 val tertiaryContainerLight = Color(0xFFDCE1FF)
 val onTertiaryContainerLight = Color(0xFF03174B)
+val surfaceVariantLight = Color(0xFFCEE5FF) //Original: 0xFFE1E2EC
+val onSurfaceVariantLight = Color(0xFF35383F)
+val outlineVariantLight = Color(0xFF001A42)
+val onSurfaceLight = Color(0xFF1A1B20)
+val errorLight = Color(0xFFBA1A1A)
 val onErrorLight = Color(0xFFFFFFFF)
 val errorContainerLight = Color(0xFFFFDAD6)
 val onErrorContainerLight = Color(0xFF410002)
@@ -135,10 +80,7 @@ val backgroundLight = Color(0xFFFAF9FF)
 val onBackgroundLight = Color(0xFF1A1B20)
 val surfaceLight = Color(0xFFFAF9FF)
 val outlineLight = Color(0xFF757780)
-
-val scrimLight = Color(0xFFEEEDF4)  //SurfaceContainer color for Bottombar & topBar
-
-
+val scrimLight = Color(0xFFEEEDF4)
 
 
 /*
