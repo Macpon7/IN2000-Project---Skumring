f90081356bc921f343674de6c39c5b8c9056b2d6
Adrian Emil Chambe-Eng <adrianch@ulrik.uio.no>
2024-05-16 16:11:18 +0200

    undo "fix"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     // Android Core
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
 
     // Jetpack Compose
